c168742d411fe3a67e7f39d443a7e459905f7622
Michael van Meerwijk <michaelvanm@gmail.com>
2021-01-21 14:46:15 +0100

    Tailwind installed

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
