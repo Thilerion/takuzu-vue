562d3bf8f4f890b2458f10d7de596ec24a02e12f
Michael van Meerwijk <michaelvanm@gmail.com>
2021-01-22 13:54:02 +0100

    Correct slide transition

--- src/components/DifficultySelect.vue
+++ src/components/DifficultySelect.vue
@@ -1,10 +1,12 @@
 <template>
 	<div class="difficulty-select">
 		<button @click="changeDifficulty(-1)" class="arrow-btn left"><ArrowLeftIcon /></button>
-		<div class="difficulty-current">
-			<div class="difficulty-label">
-				{{currentStars}}* - {{currentLabel}}
-			</div>
+		<div class="difficulty-current" :class="[`slide-${slideDirection}`]">
+			<transition name="slide-label">
+				<div class="difficulty-label" :key="currentStars">
+					{{currentStars}}* - {{currentLabel}}
+				</div>
+			</transition>
 		</div>
 		<button @click="changeDifficulty(1)" class="arrow-btn right"><ArrowRightIcon /></button>
 	</div>
@@ -31,6 +33,11 @@ export default {
 			required: true
 		}
 	},
+	data() {
+		return {
+			slideDirection: 'left'
+		}
+	},
 	computed: {
 		currentLabel() {
 			return this.labels[this.idx];
@@ -48,7 +55,15 @@ export default {
 	methods: {
 		changeDifficulty(amount) {
 			const nextIdx = (this.idx + amount + this.max) % this.max;
+			
+			if (amount < 0) {
+				this.slideDirection = 'left';
+			} else {
+				this.slideDirection = 'right';
+			}
+
 			this.$emit('update:modelValue', nextIdx + 1);
+
 		}
 	}
 };
@@ -91,5 +106,18 @@ export default {
 
 .difficulty-label {
 	@apply flex px-1 py-2 m-auto items-center justify-center absolute w-full h-full;
+}
+
+.slide-label-enter-active, .slide-label-leave-active {
+	transition: all .5s ease;
+}
+
+.slide-right > .slide-label-enter-from, .slide-left > .slide-label-leave-to {
+	transform: translateX(50px);
+	opacity: 0;
+}
+.slide-right > .slide-label-leave-to, .slide-left > .slide-label-enter-from {
+	transform: translateX(-50px);
+	opacity: 0;
 }
 </style>
