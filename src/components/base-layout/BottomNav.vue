67eb2b4f77ed424be29d96e244c80744f2057e1b
Michael van Meerwijk <michaelvanm@gmail.com>
2021-01-26 15:03:31 +0100

    Nav bar icons

--- src/components/base-layout/BottomNav.vue
+++ src/components/base-layout/BottomNav.vue
@@ -1,26 +1,55 @@
 <template>
 	<nav class="block fixed inset-x-0 bottom-0 z-10 bg-white border-t border-truegray-200 h-16 shadow-xl">
 		<div class="flex justify-between h-full">
-			<router-link class="nav-link" to="/"><span class="nav-link-text">Home</span></router-link>
-			<router-link class="nav-link" to="/tools"><span class="nav-link-text">Tools</span></router-link>
-			<router-link class="nav-link" to="/stats"><span class="nav-link-text">Statistics</span></router-link>
-			<router-link class="nav-link" to="/settings"><span class="nav-link-text">Settings</span></router-link>
-			<router-link class="nav-link" to="/menu"><span class="nav-link-text">More</span></router-link>
+			<router-link
+				class="nav-link"
+				v-for="item in menuItems"
+				:key="item.to"
+				:to="item.to"
+			>
+				<span class="material-icons">
+					{{item.icon}}
+				</span>
+				<span
+					class="nav-link-text"
+				>{{item.label}}</span>
+			</router-link>
 		</div>
 	</nav>
 </template>
 
 <script>
 export default {
-	
+	data() {
+		return {
+			menuItems: [
+				{ label: 'Home', to: '/', icon: 'home' },
+				{ label: 'Tools', to: '/tools', icon: 'construction' },
+				{ label: 'Stats', to: '/stats', icon: 'leaderboard' },
+				{ label: 'Settings', to: '/settings', icon: 'settings' },
+				{ label: 'More', to: '/menu', icon: 'more_horiz' },
+			]
+		}
+	}
 };
 </script>
 
 <style lang="postcss" scoped>
 .nav-link {
-	@apply w-full h-full focus:text-teal-500 hover:text-teal-500 justify-center text-center py-2 inline-flex items-center text-xs;
+	@apply w-full h-full focus:text-teal-500 hover:text-teal-500 justify-center text-center py-2 flex flex-col items-center text-xs text-gray-700 font-medium;
 }
 .nav-link.router-link-active {
 	@apply text-teal-700 font-semibold;
+}
+.nav-link > .material-icons {
+	@apply mb-1 text-gray-600;
+	font-size: 24px;
+}
+.router-link-active > .material-icons {
+	@apply text-teal-700;
+}
+
+.nav-link, .nav-link > .material-icons {
+	transition: color .1s ease;
 }
 </style>
