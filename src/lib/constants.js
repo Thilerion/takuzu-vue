9e5826de0618cb7cbda03a58dea2ede399b9ae81
Michael van Meerwijk <michaelvanm@gmail.com>
2021-01-27 15:15:03 +0100

    Puzzle constants

---
+++ src/lib/constants.js
@@ -0,0 +1,13 @@
+// value types
+export const ONE = '1';
+export const ZERO = '0';
+export const EMPTY = '.';
+
+export const OPPOSITE_VALUE = {
+	[ONE]: ZERO,
+	[ZERO]: ONE
+}
+
+// line types
+export const ROW = 'row';
+export const COLUMN = 'column';
