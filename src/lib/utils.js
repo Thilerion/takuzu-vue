634dbb789de24e5a1aebc99a134091fdc99de647
Michael van Meerwijk <michaelvanm@gmail.com>
2021-01-27 16:28:38 +0100

    Utils and tests

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,10 +1,73 @@
 import { EMPTY, ONE, ZERO } from "./constants";
 
+// MEMOIZE FUNCTION //
+const defaultArgsToKey = (...args) => args.join(',');
+export function memoize(fn, opts = {}) {
+	const {
+		argsToKey = defaultArgsToKey,
+		cache = {}
+	} = opts;
+	return (...a) => {
+		const key = argsToKey(...a);
+		if (cache[key] == null) {
+			const result = fn(...a);
+			cache[key] = result;
+		}
+		return cache[key];
+	}
+}
+
+// ARRAY UTILS //
 export const array2d = (width, height = width, value = null) => {
 	return Array(height).fill(null).map(() => Array(width).fill(value));
 }
 export const cloneArray2d = (arr2) => {
 	return arr2.map(row => [...row]);
 }
+export const shuffle = (arrOrig) => {
+	const arr = [...arrOrig];
+	for (let i = arr.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[arr[i], arr[j]] = [arr[j], arr[i]];
+	}
+	return arr;
+}
 
-export const isValidCellDigit = (value) => value === ONE || value === ZERO || value === EMPTY;
+
+// BOARD / CELL UTILS //
+export const isValidCellDigit = (value) => value === ONE || value === ZERO;
+
+// faster than casting to strings and comparing them
+// IMPORTANT: assumes both lines have the same length; this is always the case in this game type, as rows and columns should never be compared
+export const areLinesEqual = (a, b) => {
+	for (let i = 0; i < a.length; i++) {
+		if (a[i] !== b[i]) return false;
+	}
+	return true;
+}
+
+export const lineSizeToNumRequired = (lineSize) => {
+	return {
+		[ONE]: numRequiredOfValue(lineSize, ONE),
+		[ZERO]: numRequiredOfValue(lineSize, ZERO)
+	}
+}
+export const numRequiredOfValue = (lineSize, value) => {
+	const half = lineSize / 2;
+	return value === ONE ? Math.ceil(half) : Math.floor(half);
+}
+
+export const getCoordsForBoardSize = memoize(
+	(width, height) => {
+		const cellCoords = [];
+		for (let y = 0; y < height; y++) {
+			for (let x = 0; x < width; x++) {
+				cellCoords.push({ x, y });
+			}
+		}
+		return cellCoords;
+	},
+	{
+		argsToKey: (width, height) => `${width},${height}`
+	}
+)
---
+++ tests/unit/lib/utils.spec.js
@@ -0,0 +1,49 @@
+import { EMPTY, ONE, ZERO } from '../../../src/lib/constants';
+import { getCoordsForBoardSize, isValidCellDigit, areLinesEqual } from '../../../src/lib/utils';
+
+describe('board and puzzle utils', () => {
+	
+	describe('getCoordsForBoardSize', () => {
+
+		test('returns correct coordinates', () => {
+			const expected = [
+				{ x: 0, y: 0 },
+				{ x: 1, y: 0 },
+				{ x: 2, y: 0 },
+				{ x: 0, y: 1 },
+				{ x: 1, y: 1 },
+				{ x: 2, y: 1 },
+			];
+			const result = getCoordsForBoardSize(3, 2);
+			expect(result).toEqual(expected);
+		})
+
+		test('is memoized', () => {
+			const firstResult = getCoordsForBoardSize(4, 5);
+			const secondResult = getCoordsForBoardSize(4, 5);
+
+			expect(firstResult).toBe(secondResult);
+			expect(firstResult[0]).toBe(secondResult[0]);
+		})
+
+	})
+
+	test('isValidCellDigit', () => {
+		expect(isValidCellDigit(ONE)).toBe(true);
+		expect(isValidCellDigit(ZERO)).toBe(true);
+		expect(isValidCellDigit(EMPTY)).toBe(false);
+		expect(isValidCellDigit(false)).toBe(false);
+		expect(isValidCellDigit(true)).toBe(false);
+	})
+
+	test('are lines equal', () => {
+		expect(areLinesEqual(
+			[1, 2, 3, 4, 5],
+			[1, 2, 3, 5, 4]
+		)).toBe(false);
+		expect(areLinesEqual(
+			[true, null, -1, ONE, ZERO, EMPTY],
+			[true, null, -1, ONE, ZERO, EMPTY]
+		)).toBe(true);
+	})
+})
