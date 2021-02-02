e2c7629a949e26429039d979f3ada47efc95d9e7
Michael van Meerwijk <michaelvanm@gmail.com>
2021-02-02 11:00:30 +0100

    Size select button styled

---
+++ src/components/base-layout/BaseSelectButton.vue
@@ -0,0 +1,27 @@
+<template>
+	<button
+		class="select-btn"
+		:class="{ selected }"
+	><slot /></button>
+</template>
+
+<script>
+export default {
+	props: {
+		selected: Boolean,
+	},
+};
+</script>
+
+<style lang="postcss" scoped>
+.select-btn {
+	@apply text-sm font-bold py-1 px-4 rounded mb-2 tracking-wider bg-white text-gray-900 shadow border border-gray-300 transition-all;
+	@apply focus:outline-none;
+}
+.select-btn.selected {
+	@apply bg-teal-600 text-white border-teal-600 shadow-md;
+}
+.select-btn:disabled {
+	@apply opacity-50 bg-gray-300 shadow-none;
+}
+</style>
