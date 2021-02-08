9fb550eefd3d79765dce66e7942c37eff2a18ad6
Michael van Meerwijk <michaelvanm@gmail.com>
2021-02-08 11:26:28 +0100

    Improved button styles

--- src/components/global/BaseButton.vue
+++ src/components/global/BaseButton.vue
@@ -29,14 +29,14 @@ export default {
 
 <style lang="postcss" scoped>
 .btn {
-	@apply font-medium rounded bg-white text-gray-800 border border-gray-200 cursor-pointer justify-center px-4 py-2 text-center whitespace-nowrap select-none focus:outline-none ring-blue-600 focus:ring focus:ring-opacity-50 focus-visible:ring focus-visible:ring-opacity-90 hover-hover:hover:bg-gray-200 hover-hover:hover:border-gray-300;
+	@apply font-medium rounded bg-white text-gray-800 border border-gray-200 cursor-pointer justify-center px-4 py-2 text-center whitespace-nowrap select-none focus:outline-none ring-blue-600 focus:ring focus:ring-opacity-50 focus-visible:ring focus-visible:ring-opacity-90 hover-hover:hover:bg-gray-50 hover-hover:hover:border-gray-400 transition-all;
 }
 
 .btn-primary {
-	@apply bg-blue-500 hover:bg-blue-700 text-white border-0;
+	@apply bg-blue-500 hover-hover:hover:bg-blue-700 text-white border-0;
 }
 
 .btn-elevated {
-	@apply hover:bg-gray-100 text-gray-800 font-medium border border-gray-300 rounded shadow;
+	@apply hover-hover:hover:bg-gray-100 hover-hover:hover:shadow-md text-gray-800 font-medium border border-gray-300 rounded shadow;
 }
 </style>
