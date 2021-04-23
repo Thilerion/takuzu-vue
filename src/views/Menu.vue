608ce211bbd9d7947b5e7b31c7e9b0440b27740a
Michael van Meerwijk <michaelvanm@gmail.com>
2021-04-23 17:49:14 +0200

    NPM package version as env variable

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,15 +1,23 @@
 <template>
 	<div>
-		<PageHeader>More...</PageHeader>
-		<router-link custom to="/showcase" v-slot="{ navigate }">
+		<PageHeader>Takuzu</PageHeader>
+		<router-link v-if="enableComponentShowcase" custom to="/showcase" v-slot="{ navigate }">
 			<BaseButton @click="navigate" element="a">Open component showcase</BaseButton>
 		</router-link>
+		<div class="text-center p-2 text-sm text-gray-600">App version: {{ appVersion }}</div>
 	</div>
 </template>
 
 <script>
 export default {
-	
+	computed: {
+		enableComponentShowcase() {
+			return process.env.NODE_ENV !== 'development';
+		},
+		appVersion() {
+			return process.env.VUE_APP_VERSION;
+		}
+	}
 };
 </script>
 
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const WorkerPlugin = require('worker-plugin');
 
+const packageAppVersion = require('./package.json').version;
+process.env.VUE_APP_VERSION = packageAppVersion;
+
 module.exports = {
 	configureWebpack: {
 		output: {
