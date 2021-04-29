26fda833c01ba5589521c572ff8ab35d8cd6ed7d
Michael van Meerwijk <michaelvanm@gmail.com>
2021-04-29 19:28:07 +0200

    Add average playtime to stats page

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -5,25 +5,51 @@
 			<h2 class="text-lg font-medium">Overall</h2>
 			<span>Puzzles solved:</span>
 			<span>{{puzzlesSolved}}</span>
+			<span>Average time per puzzle:</span>
+			<span>{{msToMinSec(averageTime)}}</span>
 		</section>
 	</div>
 </template>
 
 <script>
 import { statsQueries } from '@/services/stats';
+import { puzzleHistoryDb } from '@/services/stats/db';
 export default {
 	data() {
 		return {
-			puzzlesSolved: null
+			puzzlesSolved: null,
+			averageTime: null,
 		}
 	},
 	methods: {
 		getPuzzlesSolved() {
 			return statsQueries.numSolved();
 		},
+		async getAverageTime() {
+			let num = 0;
+			let time = 0;
+			await puzzleHistoryDb
+				.orderBy('timeElapsed')
+				.eachKey((timeElapsed) => {
+					num += 1;
+					time += (timeElapsed * 1);
+				});
+			console.log({num, time});
+			return time / num;			
+		},
 		async getInitialData() {
 			this.puzzlesSolved = await this.getPuzzlesSolved();
-		}
+			this.averageTime = await this.getAverageTime();
+		},
+		msToMinSec(ms) {
+			const format = val => `0${Math.floor(val)}`.slice(-2);
+
+			const fullSeconds = Math.floor(ms / 1000);
+
+			const minutes = fullSeconds / 60;
+			const seconds = fullSeconds % 60;
+			return `${format(minutes)}:${format(seconds)}`;
+		},
 	},
 	beforeMount() {
 		this.getInitialData();
