2fb8e4b319b29d01e6d0e2f2a45a7e9fbd0813cb
Michael van Meerwijk <michaelvanm@gmail.com>
2021-05-04 15:50:49 +0200

    Improved MainMenu/Home component

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,7 +1,7 @@
 <template>
-	<div class="pb-20 pt-14 relative">
-		<div class="text-right absolute right-1 font-number opacity-30">100110110010</div>
-		<h1 class="text-6xl font-extrabold">Takuzu</h1>
+	<div class="py-2 relative text-center mx-auto">
+		<div class="text-right absolute right-1 font-number opacity-30">10011011001001</div>
+		<h1 class="text-7xl font-extrabold">Takuzu</h1>
 	</div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,73 +1,75 @@
-	<template>
-	<div class="main-menu h-full flex-1 flex flex-col items-center justify-center text-center">
-		<AppTitle />
-		<nav class="flex flex-col w-full px-6 space-y-4">
-			<div class="routes main-routes flex flex-col space-y-4">
-				<router-link
-					v-if="canContinue"
-					:to="{ path: '/play', query: { continue: true}}"
-					custom
-					v-slot="{ navigate }"
-				><BaseButton
-					@click="navigate"
-					class="btn-primary text-base uppercase shadow-md"
-				>Continue</BaseButton></router-link>
+<template>
+	<div class="main-menu h-full min-h-full flex flex-col text-center flex-1 space-y-14">
+		<div class="flex flex-col title-wrapper justify-end bg-opacity-20">
+			<app-title/>
+			<!-- <span>Title here</span> -->
+		</div>
+		<div class="flex flex-col items-center justify-start route-wrapper space-y-4">
+			<router-link
+				v-if="canContinue"
+				:to="{ path: '/play', query: { continue: true}}"
+				custom
+				v-slot="{ navigate }"
+			><BaseButton
+				@click="navigate"
+				class="btn-primary text-base uppercase shadow-md route-btn route-primary"
+			>Continue</BaseButton></router-link>
 
-				<router-link
-					to="/play"
-					custom
-					v-slot="{ navigate }"
-				><BaseButton
-					@click="navigate"
-					class="text-base uppercase"
-					:class="{'btn-primary': !canContinue, 'shadow-md': !canContinue}"
-				>New Game</BaseButton></router-link>
+			<router-link
+				to="/play"
+				custom
+				v-slot="{ navigate }"
+			><BaseButton
+				@click="navigate"
+				class="text-base uppercase route-btn route-primary"
+				:class="{'btn-primary': !canContinue, 'shadow-md': !canContinue}"
+			>New Game</BaseButton></router-link>
 
+			<div class="pt-4">
+				<router-link to="/how-to-play" class="route-btn route-secondary">How to play</router-link>
 			</div>
-			<div class="routes secondary-routes flex flex-col flex-1">
-				<router-link to="/how-to-play" class="py-3">How to play</router-link>
-			</div>			
-		</nav>
+		</div>
 	</div>
 </template>
 
 <script>
-import { hasCurrentSavedGame } from '@/services/save-game'
-import AppTitle from '@/components/AppTitle';
+import { hasCurrentSavedGame } from '@/services/save-game';
+import AppTitle from '@/components/AppTitle.vue';
 import BaseButton from '@/components/global/BaseButton';
-
 export default {
-	name: 'MainMenu',
-	components: {
-		AppTitle,
-		BaseButton,
-	},
+	components: { AppTitle, BaseButton },
 	data() {
 		return {
-			canContinue: false,
+			canContinue: null,
 		}
 	},
 	beforeMount() {
 		this.canContinue = hasCurrentSavedGame();
-	},
-}
+	}
+};
 </script>
 
 <style lang="postcss" scoped>
 .main-menu {
-	background:url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25'%3E%3Cdefs%3E%3Cpattern id='pattern' width='52' height='52' viewBox='0 0 40 40' patternUnits='userSpaceOnUse' patternTransform='rotate(28)'%3E%3Crect id='pattern-background' width='400%25' height='400%25' fill='rgba(247, 250, 252,0.4)'%3E%3C/rect%3E%3Cpath fill='rgba(237, 242, 247,0.4)' d='M0 0L20 0L20 20L0 20L0 0zM6 6L6 14L17.9 17.9L14 6zM20 20L40 20L40 40L20 40L20 20zM22.1 22.1L26 34L34 34L34 26z'%3E%3C/path%3E%3Cpath fill='rgba(237, 242, 247,0.3)' d='M26 6L34 6L34 14L26 14zM6 26L14 26L14 34L6 34z'%3E%3C/path%3E%3C/pattern%3E%3C/defs%3E%3Crect fill='url(%23pattern)' height='100%25' width='100%25'%3E%3C/rect%3E%3C/svg%3E")
+	background:url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25'%3E%3Cdefs%3E%3Cpattern id='pattern' width='52' height='52' viewBox='0 0 40 40' patternUnits='userSpaceOnUse' patternTransform='rotate(28)'%3E%3Crect id='pattern-background' width='400%25' height='400%25' fill='rgba(247, 250, 252,0.4)'%3E%3C/rect%3E%3Cpath fill='rgba(237, 242, 247,0.4)' d='M0 0L20 0L20 20L0 20L0 0zM6 6L6 14L17.9 17.9L14 6zM20 20L40 20L40 40L20 40L20 20zM22.1 22.1L26 34L34 34L34 26z'%3E%3C/path%3E%3Cpath fill='rgba(237, 242, 247,0.3)' d='M26 6L34 6L34 14L26 14zM6 26L14 26L14 34L6 34z'%3E%3C/path%3E%3C/pattern%3E%3C/defs%3E%3Crect fill='url(%23pattern)' height='100%25' width='100%25'%3E%3C/rect%3E%3C/svg%3E");
 }
 
-.main-routes {
-	@apply text-2xl tracking-wide items-center;
-	line-height: 3rem;
+.title-wrapper {
+	@apply flex-1;
+	flex-grow: 2;
+	max-height: 35vh;
 }
-.secondary-routes {
-	@apply text-lg;
-	line-height: 2.25rem;
+.route-wrapper {
+	@apply flex-auto;
 }
-
-.main-menu .btn {
-	@apply font-normal py-4 w-2/3;
+.route-btn {
+	@apply w-2/3 font-normal;
+	max-width: 18rem;
+}
+.route-btn.route-primary {
+	@apply tracking-wider py-3;
+}
+.route-btn.route-secondary {
+	@apply tracking-wide;
 }
 </style>
