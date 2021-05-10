98ac713cf977d1c54b17e06cc9a3e6c88d67f01b
Michael van Meerwijk <michaelvanm@gmail.com>
2021-05-10 12:09:20 +0200

    Dev mode toggle for additional options

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,21 +1,53 @@
 <template>
-	<div>
+	<div class="flex flex-col flex-1">
 		<PageHeader>Takuzu</PageHeader>
-		<router-link v-if="enableComponentShowcase" custom to="/showcase" v-slot="{ navigate }">
-			<BaseButton @click="navigate" element="a">Open component showcase</BaseButton>
-		</router-link>
-		<div class="text-center p-2 text-sm text-gray-600">App version: {{ appVersion }}</div>
+		<div class="flex-1 px-8 text-center space-y-4">
+			<button
+				@click="increaseDevModeClickCounter"
+				:disabled="isDevModeEnabled"
+				class="block mx-auto"
+			>
+				<div
+					class="text-center p-2 text-sm text-gray-600"
+				>App version: {{ appVersion }}</div>
+			</button>
+			<router-link v-if="isDevModeEnabled" custom to="/showcase" v-slot="{ navigate }">
+				<BaseButton @click="navigate">Open component showcase</BaseButton>
+			</router-link>
+			<BaseButton @click="disableDevMode" v-if="isDevModeEnabled">Disable dev mode</BaseButton>
+		</div>
 	</div>
 </template>
 
 <script>
 export default {
+	data() {
+		return {
+			devModeClickCounter: 0,
+		}
+	},
 	computed: {
-		enableComponentShowcase() {
-			return process.env.NODE_ENV === 'development';
-		},
 		appVersion() {
 			return process.env.VUE_APP_VERSION;
+		},
+		isDevModeEnabled() {
+			return this.$store.state.devMode;
+		}
+	},
+	methods: {
+		increaseDevModeClickCounter() {
+			this.devModeClickCounter += 1;
+			if (this.devModeClickCounter >= 5) {
+				this.enableDevMode();
+			}
+		},
+		enableDevMode() {
+			this.$store.commit('setDevMode', true);
+			this.devModeClickCounter = 0;
+		},
+		disableDevMode() {
+			this.$store.commit('setDevMode', false);
+			this.devModeClickCounter = 0;
 		}
 	}
 };
