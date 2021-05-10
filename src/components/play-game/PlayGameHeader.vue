895093d78b74545ca4313a9fc45d0b0e392383e8
Michael van Meerwijk <michaelvanm@gmail.com>
2021-05-10 12:27:08 +0200

    Copy puzzle to clipboard option in devMode

--- src/components/play-game/PlayGameHeader.vue
+++ src/components/play-game/PlayGameHeader.vue
@@ -23,6 +23,16 @@
 						</label>
 					</BaseDropdownItem>
 					<BaseDropdownDivider/>
+
+					<template v-if="devModeEnabled">
+						<BaseDropdownItem @click="copyPuzzleString">
+							<span class="material-icons opacity-90 text-base">content_copy</span>
+							<span class="ml-3 mt-px">Copy board string</span>
+							<span v-if="copyError" class="ml-2 text-xs text-red-700 mt-px">{{copyError}}</span>
+						</BaseDropdownItem>
+						<BaseDropdownDivider/>
+					</template>
+
 					<BaseDropdownItem @click="goToSettings"
 					>
 						<span class="material-icons opacity-90 text-base">settings</span>
@@ -39,7 +49,15 @@ export default {
 	props: [
 		'columns', 'rows',
 	],
+	data() {
+		return {
+			copyError: null
+		}
+	},
 	computed: {
+		devModeEnabled() {
+			return this.$store.state.devMode;
+		},
 		showTimer: {
 			get() {
 				return this.$store.state.settings.showTimer;
@@ -50,12 +68,28 @@ export default {
 					value
 				});
 			}
+		},
+		board() {
+			return this.$store.state.game.board;
 		}
 	},
 	methods: {
 		goToSettings() {
 			this.$refs.dropdown.closeDropdownMenu();
 			this.$emit('open-settings');
+		},
+		async copyPuzzleString() {
+			const boardStr = this.board.toBoardString();
+			console.log({boardStr});
+			try {
+				await navigator.clipboard.writeText(boardStr);
+				console.log('Successfully copied string to clipboard!');
+				this.$refs.dropdown.closeDropdownMenu();
+			} catch(e) {
+				console.warn(e);
+				console.log('Could not copy puzzle string to clipboard.');
+				this.copyError = "Error...";
+			}
 		}
 	}
 };
