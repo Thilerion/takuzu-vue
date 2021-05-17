60b64d6206e1092667bf8ba29685f3dc3d7b52fa
Michael van Meerwijk <michaelvanm@gmail.com>
2021-05-17 13:31:51 +0200

    Date utils unit tests

---
+++ tests/unit/utils/date.utils.spec.js
@@ -0,0 +1,25 @@
+import * as dateUtils from '@/utils/date.utils';
+
+describe('date utilities', () => {
+	test('isSameDay', () => {
+		expect(dateUtils.isSameDay(new Date(), new Date())).toBe(true);
+
+		const dateA = new Date('2011-10-10T00:00:01');
+		const dateB = new Date('2011-10-09T23:59:59');
+		const dateC = new Date('2011-10-09T00:00:01');
+
+		expect(dateUtils.isSameDay(dateA, dateB)).toBe(false);
+		expect(dateUtils.isSameDay(dateA, dateC)).toBe(false);
+		expect(dateUtils.isSameDay(dateB, dateC)).toBe(true);
+	})
+
+	test('isNextDay', () => {
+		expect(dateUtils.isNextDay(new Date(), new Date())).toBe(false);
+
+		const dateA = new Date('2011-10-10T00:00:01');
+		const dateB = new Date('2011-10-09T23:59:59');
+
+		expect(dateUtils.isNextDay(dateA, dateB)).toBe(false);
+		expect(dateUtils.isNextDay(dateB, dateA)).toBe(true);
+	})
+})
