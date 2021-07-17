0c6cc781091d2f74913bf26dd28cfc8a8bb4089a
Thilerion <michaelvanm@gmail.com>
2021-07-17 21:27:17 +0200

    Fix puzzle generation for small and hard puzzles

--- src/workers/generate-puzzle.worker.js
+++ src/workers/generate-puzzle.worker.js
@@ -6,15 +6,24 @@ const minMaskedRatioQuality = 0.94;
 const minSymbolDistributionQuality = 0.6;
 
 const tryDurations = [250, 1000, 1750, 2000];
-const maxTries = tryDurations.length;
+const totalMaxDuration = tryDurations.reduce((acc, val) => acc + val, 0);
+const maxTries = 20;
 
 function createPuzzle(puzzleConfig = {}) {
 	const { width, height, difficulty } = puzzleConfig;
 
+	const start = performance.now();
+	const end = start + totalMaxDuration;
+	const shouldEnd = () => performance.now() >= end;
+
 	const optimalMaskedRatio = getOptimalMaskRatio(width, height, difficulty);
 
 	for (let i = 0; i < maxTries; i++) {
-		const maxTime = tryDurations[i];
+		if (shouldEnd()) break;
+		let maxTime = 3000;
+		if (i < tryDurations.length) {
+			maxTime = tryDurations[i];
+		}
 		const genResult = createBoardAndMaskOnce({ optimalMaskedRatio, width, height, difficulty }, maxTime);
 		const { success, error } = genResult;
 
@@ -22,10 +31,10 @@ function createPuzzle(puzzleConfig = {}) {
 			const { solution, board, quality } = genResult;
 			return { solution, board, quality };
 		} else if (error) {
-			console.warn(`Puzzle generation, try [${i}/4]: ${error}`);
+			console.warn(`Puzzle generation, try [${i}/${maxTries}]: ${error}`);
 		}
 	}
-	console.warn('After 4 tries generating boards and masks: timeout reached.');
+	console.warn('Timeout reached generating puzzle.');
 	return;
 }
 
