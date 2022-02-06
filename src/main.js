5f31c5480ab66e9d75169f86db4e761068496156
Thilerion <michaelvanm@gmail.com>
2022-02-06 11:56:23 +0100

    Immediate SW update

--- src/main.js
+++ src/main.js
@@ -7,13 +7,13 @@ import { registerGlobalComponents } from './global-components.js';
 import { registerSW } from 'virtual:pwa-register';
 
 const SW_UPDATE_INTERVAL_MS = 60 * 60 * 1000;
-
 const updateSW = registerSW({
-  onRegistered(r) {
-    r && setInterval(() => {
-		r.update();
-    }, SW_UPDATE_INTERVAL_MS)
-  }
+	immediate: true,
+	onRegistered(r) {
+		r && setInterval(() => {
+			r.update();
+		}, SW_UPDATE_INTERVAL_MS)
+	}
 })
 
 // const pkgVersion = import.meta.env.PACKAGE_VERSION;
