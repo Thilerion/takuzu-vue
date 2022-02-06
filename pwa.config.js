94b3faae333ba74751f5315329daa29017bb12ba
Thilerion <michaelvanm@gmail.com>
2022-02-06 15:00:42 +0100

    Use env variables for title and manifest name

--- pwa.config.js
+++ pwa.config.js
@@ -1,34 +1,44 @@
-export const vitePwaConfig = {
-	registerType: 'autoUpdate',
-	includeAssets: ['favicon.ico', 'robots.txt', 'safari-pinned-tab.svg', 'apple-touch-icon.png'],
-	manifest: {
-		name: 'Takuzu Vue',
-		short_name: 'Takuzu Vue',
-		description: 'Description of your app',
-		theme_color: "#4DBA87",
-		icons: [
-			{
-				"src": "/android-chrome-192x192.png",
-				"sizes": "192x192",
-				"type": "image/png"
-			},
-			{
-				"src": "/android-chrome-512x512.png",
-				"sizes": "512x512",
-				"type": "image/png"
-			},
-			{
-				"src": "/android-chrome-maskable-192x192.png",
-				"sizes": "192x192",
-				"type": "image/png",
-				"purpose": "maskable"
-			},
-			{
-				"src": "/android-chrome-maskable-512x512.png",
-				"sizes": "512x512",
-				"type": "image/png",
-				"purpose": "maskable"
-			}
-		]
+export const createVitePwaConfig = ({
+	name = 'Takuzu Vue',
+	short_name = 'Takuzu Vue'
+}) => {
+	/**
+	 * @type {import('vite-plugin-pwa').VitePWAOptions}
+	 */
+	const config = {
+		includeAssets: ['favicon.ico', 'robots.txt', 'safari-pinned-tab.svg', 'apple-touch-icon.png'],
+		manifest: {
+			name,
+			short_name,
+			description: 'Binary puzzle, also called takuzu or binairo.',
+			theme_color: "#4DBA87",
+			orientation: 'portrait-primary',
+			background_color: '#f8fafc',
+			icons: [
+				{
+					"src": "/android-chrome-192x192.png",
+					"sizes": "192x192",
+					"type": "image/png"
+				},
+				{
+					"src": "/android-chrome-512x512.png",
+					"sizes": "512x512",
+					"type": "image/png"
+				},
+				{
+					"src": "/android-chrome-maskable-192x192.png",
+					"sizes": "192x192",
+					"type": "image/png",
+					"purpose": "maskable"
+				},
+				{
+					"src": "/android-chrome-maskable-512x512.png",
+					"sizes": "512x512",
+					"type": "image/png",
+					"purpose": "maskable"
+				}
+			]
+		}
 	}
-};
+	return config;
+}
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
 import { resolve } from 'path'
 
 import vue from '@vitejs/plugin-vue';
@@ -6,36 +6,62 @@ import { VitePWA } from 'vite-plugin-pwa';
 import Icons from 'unplugin-icons/vite';
 import IconsResolver from 'unplugin-icons/resolver';
 import Components from 'unplugin-vue-components/vite';
+import copy from 'rollup-plugin-copy';
 
-import { vitePwaConfig } from './pwa.config.js'
+import { createVitePwaConfig } from './pwa.config.js'
 
-export default defineConfig({
-	plugins: [
-		vue(),
-		VitePWA(vitePwaConfig),
-		Components({
-			extensions: ['vue'],
-			include: [/\.vue$/, /\.vue\?vue/],
-			resolvers: [
-				IconsResolver({
-					prefix: 'icon',
-					alias: {
-						'his': 'heroicons-solid'
-					}
-				})
-			]
-		}),
-		Icons({
-			defaultClass: 'base-icon',
-		})
-	],
-	resolve: {
-		alias: {
-			'@': resolve(__dirname, 'src'),
+export default defineConfig(({ command, mode }) => {
+	// load env variables from relevant env file to set manifest app name
+	process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+	// if building for production, create a 200.html fallback for nested routes
+	const copyFallback200 = command === 'build' ? copy({
+		hook: 'writeBundle',
+		targets: [
+			{ src: 'dist/index.html', dest: 'dist/', rename: '200.html' }
+		]
+	}) : undefined;
+
+	return {
+		plugins: [
+			vue(),
+			VitePWA(createVitePwaConfig({
+				name: process.env.VITE_APP_NAME,
+				short_name: process.env.VITE_APP_SHORT_NAME
+			})),
+			Components({
+				extensions: ['vue'],
+				include: [/\.vue$/, /\.vue\?vue/],
+				resolvers: [
+					IconsResolver({
+						prefix: 'icon',
+						alias: {
+							'his': 'heroicons-solid'
+						}
+					})
+				]
+			}),
+			Icons({
+				defaultClass: 'base-icon',
+			}),
+			copyFallback200
+		],
+		resolve: {
+			alias: {
+				'@': resolve(__dirname, 'src'),
+			},
 		},
-	},
-	define: {
-		__BUILD_DATE__: JSON.stringify(new Date().toISOString().slice(0, 16)),
-		__PKG_VERSION__: JSON.stringify(process.env.npm_package_version)
+		define: {
+			__BUILD_DATE__: JSON.stringify(
+				(() => {
+					// include correction for timezone offset in ISO string
+					const date = new Date();
+					const offset = date.getTimezoneOffset() * 60000;
+					const offsetDate = new Date(date.getTime() - offset);
+					return offsetDate.toISOString().slice(0, 16);
+				})()
+			),
+			__PKG_VERSION__: JSON.stringify(process.env.npm_package_version),
+		}
 	}
 })
