81466c45862a39936ca6e3a3902c074db0923a06
Thilerion <michaelvanm@gmail.com>
2022-03-07 18:16:59 +0100

    FreePlay view improvements: fixed footer; saved puzzle warning

--- src/views/FreePlay2.vue
+++ src/views/FreePlay2.vue
@@ -1,63 +1,68 @@
 <template>
-	<div class="freeplay flex flex-col overflow-x-hidden pb-28 min-h-full overflow-y-auto">
-		<PageHeader class="flex-shrink-0" small>
-			<template #default>New Game</template>
-		</PageHeader>
-		<div class="main flex-1 w-full sm:w-[500px] mx-auto flex-shrink-0 my-auto">
-			<div class="pb-3">
-			<h2 class="text-base font-medium mb-1 text-gray-700/90 ml-4 tracking-wide">Difficulty</h2>
-			<div class="content-block difficulty-select mb-6">
-				<DifficultySelect
-					@decrease="decreaseDifficulty"
-					@increase="increaseDifficulty"
-					:labels="DIFFICULTY_LABELS"
-					:difficulty="selectedDifficulty"
-				/>
+	<div class="h-vh overflow-y-hidden flex flex-col">
+		<div class="flex-1 overflow-y-auto pb-4 flex flex-col">
+			<PageHeader class="flex-shrink-0" small>
+				<template #default>New Game</template>
+			</PageHeader>
+			<div class="main flex-1 w-full sm:w-[500px] mx-auto flex-shrink-0 my-auto justify-around">
+			<div class="pb-2">
+				<h2 class="text-base font-medium mb-1 text-gray-700/90 ml-4 tracking-wide">Difficulty</h2>
+				<div class="content-block difficulty-select">
+					<DifficultySelect
+						@decrease="decreaseDifficulty"
+						@increase="increaseDifficulty"
+						:labels="DIFFICULTY_LABELS"
+						:difficulty="selectedDifficulty"
+					/>
+				</div>
+				</div>
+				<div>
+				<h2 class="text-base font-medium mb-1 text-gray-700/90 ml-4 tracking-wide">Puzzle size</h2>
+				<div
+					class="content-block flex-shrink-0 rounded shadow-sm"
+				>
+					<div class="mb-1 font-medium text-sm text-gray-500">Normal</div>
+					<PuzzleDimensionsBlock
+						:presets="squarePresets"
+						:selected-dimensions="selectedDimensions"
+						@select="selectPreset"
+					/>
+					<div class="mb-1 font-medium text-sm text-gray-500">Tall</div>
+					<PuzzleDimensionsBlock
+						:presets="rectPresets"
+						:selected-dimensions="selectedDimensions"
+						@select="selectPreset"
+					/>
+					<div class="mb-1 font-medium text-sm text-gray-500">Odd</div>
+					<PuzzleDimensionsBlock
+						:presets="oddPresets"
+						:selected-dimensions="selectedDimensions"
+						@select="selectPreset"
+					/>
+				</div>
+				</div>
 			</div>
+		</div>
+		<div class="w-full bg-gray-50 border-t border-gray-200 px-4 pb-4 pt-2 footer-wrapper space-y-2">
+			<div v-if="hasCurrentSavedGame" class="text-xs text-orange-700">
+				<p>Starting a new puzzle will overwrite your current puzzle in progress.</p>
 			</div>
-			<div>
-			<h2 class="text-base font-medium mb-1 text-gray-700/90 ml-4 tracking-wide">Puzzle size</h2>
-			<div
-				class="content-block flex-shrink-0 rounded shadow-sm"
-			>
-				<div class="mb-1 font-medium text-sm text-gray-500">Normal</div>
-				<PuzzleDimensionsBlock
-					:presets="squarePresets"
-					:selected-dimensions="selectedDimensions"
-					@select="selectPreset"
-				/>
-				<div class="mb-1 font-medium text-sm text-gray-500">Tall</div>
-				<PuzzleDimensionsBlock
-					:presets="rectPresets"
-					:selected-dimensions="selectedDimensions"
-					@select="selectPreset"
-				/>
-				<div class="mb-1 font-medium text-sm text-gray-500">Odd</div>
-				<PuzzleDimensionsBlock
-					:presets="oddPresets"
-					:selected-dimensions="selectedDimensions"
-					@select="selectPreset"
-				/>
-			</div>
-			</div>
-
-			<div class="fixed bottom-0 inset-x-0 p-4 bg-gray-50 border-t border-gray-200 shadow-lg">
-				<StartGameButton
-					@click="createGame"
-					:size="selectedDimensions"
-					:difficulty-label="selectedDifficultyLabel"
-					:difficulty-stars="selectedDifficulty"
-					:disabled="disableStartButton"
-					:loading="puzzleIsLoading"
-				/>
-			</div>
+			<StartGameButton
+				class="first:mt-4"
+				@click="createGame"
+				:size="selectedDimensions"
+				:difficulty-label="selectedDifficultyLabel"
+				:difficulty-stars="selectedDifficulty"
+				:disabled="disableStartButton"
+				:loading="puzzleIsLoading"
+			/>
 		</div>
 	</div>
 </template>
 
 <script setup>
 import { DIFFICULTY_LABELS, PRESET_BOARD_SIZES } from '@/config.js';
-import { computed, ref, watch, watchEffect } from 'vue';
+import { computed, onBeforeUpdate, onMounted, ref, watch, watchEffect } from 'vue';
 import PageHeader from '../components/global/base-layout/PageHeader.vue';
 import StartGameButton from '../components/new-game2/StartGameButton.vue';
 import PuzzleDimensionsBlock from '../components/new-game2/PuzzleDimensionsBlock.vue';
@@ -65,6 +70,10 @@ import { onBeforeRouteLeave, useRouter } from 'vue-router';
 import { useStore } from 'vuex';
 import { usePreviousSelection } from '../components/new-game2/usePreviousSelection.js';
 import DifficultySelect from '../components/new-game2/DifficultySelect.vue';
+
+import { useSavedPuzzle } from '@/services/useSavedPuzzle.js';
+const { hasCurrentSavedGame } = useSavedPuzzle();
+// display warning message if creating a new game will overwrite the currently saved puzzle
 
 const previousSelection = usePreviousSelection();
 
@@ -188,7 +197,7 @@ async function createGame() {
 
 <style scoped>
 .freeplay {
-	height: var(--vh-total);
+	padding-bottom: calc(var(--footer-size) * 1px + 1rem);
 	z-index: 100;
 	width: 100vw;
 	position: fixed;
@@ -197,7 +206,7 @@ async function createGame() {
 }
 
 .main {
-	@apply px-4 flex flex-col justify-center;
+	@apply px-4 flex flex-col;
 }
 
 .content-block {
@@ -205,5 +214,9 @@ async function createGame() {
 }
 .difficulty-select {
 	@apply py-0;
+}
+
+.footer-wrapper {
+	box-shadow: 0 -12px 14px -16px rgba(0,0,0,.2);
 }
 </style>
