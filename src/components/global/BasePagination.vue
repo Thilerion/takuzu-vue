847bb53c26e8bd7ce9e225ff1ad6bfa3e95be1d8
Thilerion <michaelvanm@gmail.com>
2022-03-26 15:57:57 +0100

    Basic history pagination

---
+++ src/components/global/BasePagination.vue
@@ -0,0 +1,61 @@
+<template>
+	<div class="flex flex-row text-sm py-3 gap-4 justify-center items-center px-2 mt-2">
+
+		<div class="w-10"><button class="bg-white border rounded h-8 px-2" @click="goToPage(-Infinity)" v-if="modelValue > 0">First</button></div>
+
+		<div class="w-10"
+		><button
+			class="bg-white border rounded h-8 px-2"
+			@click="changePage(-1)" v-if="modelValue > 0"
+		>Left</button></div>
+
+		<div
+			class="flex-1 text-center"
+		>Page {{modelValue + 1}} of {{maxPage + 1}}</div>
+
+		<div class="w-10"
+		><button
+			class="bg-white border rounded h-8 px-2"
+			@click="changePage(1)" v-if="modelValue < maxPage"
+		>Right</button></div>
+
+		<div class="w-10"><button class="bg-white border rounded h-8 px-2" @click="goToPage(Infinity)" v-if="modelValue < maxPage - 1">Last</button></div>
+	</div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+	length: {
+		type: Number,
+		required: true
+	},
+	pageSize: {
+		type: Number,
+		default: 10
+	},
+	modelValue: {
+		type: Number,
+		required: true
+	}
+})
+const emit = defineEmits(['update:modelValue']);
+
+const maxPage = computed(() => {
+	return Math.ceil(props.length / props.pageSize) - 1;
+})
+
+const goToPage = (value) => {
+	const newPage = Math.max(0, Math.min(maxPage.value, value));
+	emit('update:modelValue', newPage);
+}
+const changePage = (increment) => {
+	const newPage = Math.max(0, Math.min(maxPage.value, props.modelValue + increment));
+	emit('update:modelValue', newPage);
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/PuzzleHistoryView.vue
+++ src/views/PuzzleHistoryView.vue
@@ -2,26 +2,54 @@
 	<div class="flex flex-col overflow-y-auto">
 		<PageHeader>Puzzle history</PageHeader>
 		<div class="content flex-1">
-			<div class="list divide-y border-y">
-				<HistoryListItem
-					v-for="item in shownItems"
-					:key="item.id"
-					v-bind="item"
-				></HistoryListItem>
+			<div class="flex flex-row items-center gap-3 text-sm">
+				<div>Per page: </div><select v-model.number="perPage" class="form-select text-black rounded border border-gray-400 py-1 pr-5 pl-1 text-sm">
+				<option :value="10">10</option>
+				<option :value="20">20</option>
+				<option :value="50">50</option>
+				<option :value="100">100</option>
+			</select>
 			</div>
+			<div class="flex flex-row">
+				<button @click="sortBy = 'newest'">Newest</button>
+				<button @click="sortBy = 'oldest'">Oldest</button>
+			</div>
+			<div ref="anchorEl"></div>
+			<BasePagination v-model="page" :length="clonedItems.length" :page-size="perPage" />
+				<div class="list divide-y border-y">
+					<transition-group name="list-change">
+					<HistoryListItem
+						v-for="item in shownItems"
+						:key="item.id"
+						v-bind="item"
+					></HistoryListItem>
+					</transition-group>
+				</div>
 		</div>
+		<BasePagination v-model="page" :length="clonedItems.length" :page-size="perPage" />
 	</div>
 </template>
 
 <script>
 
 const sortDateNewest = (a, b) => {
-	if (a < b) return -1;
-	else if (a > b) return 1;
+	if (a.date < b.date) return 1;
+	else if (a.date > b.date) return -1;
 	return 0;
 }
 const sortDateOldest = (a, b) => {
 	return sortDateNewest(b, a);
+}
+
+const sortFns = {
+	newest: sortDateNewest,
+	oldest: sortDateOldest
+}
+
+function sortItems(items, sortBy = 'newest') {
+	const fn = sortFns[sortBy];
+	items.sort(fn);
+	return items;
 }
 
 </script>
@@ -32,6 +60,8 @@ import { useStatisticsStore2 } from '@/stores/statistics2.js';
 import { storeToRefs } from 'pinia';
 import { ref, computed, watchEffect, watch, onBeforeMount } from 'vue';
 import HistoryListItem from '@/components/statistics2/HistoryListItem.vue';
+import BasePagination from '@/components/global/BasePagination.vue';
+import { useRoute, useRouter } from 'vue-router';
 
 const statsStore = useStatisticsStore2();
 
@@ -42,26 +72,55 @@ const { historyItems } = storeToRefs(statsStore);
 
 const clonedItems = ref([...historyItems.value]);
 
-const sortBy = ref({
-	key: 'date',
-	type: 'newest'
-});
-const sortFn = computed(() => {
-	if (sortBy.value.key === 'date') {
-		if (sortBy.value.type === 'newest') return sortDateNewest;
-		else if (sortBy.value.type === 'oldest') return sortDateOldest;
-	}
-})
+const sortBy = ref('newest');
 
-watchEffect(() => {
-	clonedItems.value = clonedItems.value.sort(sortFn.value);
-})
-watch(historyItems, (value) => {
-	clonedItems.value = [...value];
+watch([historyItems, sortBy], ([value, sortBy]) => {
+	clonedItems.value = sortItems([...value], sortBy);
 })
 
 const page = ref(0);
 const perPage = ref(10);
+
+const router = useRouter();
+const route = useRoute();
+
+onBeforeMount(() => {
+	const { query } = route;
+	console.log({ query});
+	if (query.page) {
+		page.value = Number(query.page);
+	}
+	if (query.perPage) {
+		perPage.value = Number(query.perPage);
+	}
+	if (query.sortBy) {
+		sortBy.value = query.sortBy;
+	}
+})
+
+watchEffect(() => {
+	const query = {
+		...route.query,
+		page: page.value,
+		perPage: perPage.value,
+		sortBy: sortBy.value
+	};
+	const name = route.name;
+	router.replace({
+		name,
+		query
+	});
+})
+
+const anchorEl = ref(null);
+watch(page, () => {
+	if (anchorEl.value != null) {
+		anchorEl.value.scrollIntoView({
+			behavior: 'smooth',
+			block: 'nearest'
+		})
+	}
+})
 
 const shownItems = computed(() => {
 	const idx = page.value * perPage.value;
@@ -70,5 +129,15 @@ const shownItems = computed(() => {
 </script>
 
 <style scoped>
+.list-change-leave-active {
+	transition: opacity 0.3s ease;
+}
 
+.list-change-enter-active {
+	transition: opacity 0.15s ease 0.25s;
+}
+
+.list-change-enter-from, .list-change-leave-to {
+	opacity: 0;
+}
 </style>
