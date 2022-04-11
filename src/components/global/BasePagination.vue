57ef5fe754e60f0321eccb2623829db76d38a60f
Thilerion <michaelvanm@gmail.com>
2022-04-11 19:21:34 +0200

    Fix pagination bugs and improve styling

--- src/components/global/BasePagination.vue
+++ src/components/global/BasePagination.vue
@@ -1,25 +1,25 @@
 <template>
 	<div class="flex flex-row text-sm py-2 gap-4 justify-center items-center px-2">
 
-		<div class="w-10"><button class="bg-white border rounded h-8 px-2" @click="goToPage(-Infinity)" v-if="modelValue > 0">First</button></div>
+		<div class="w-10"><button class="bg-white border rounded h-8 px-2" @click="goToPage(-Infinity)" v-if="modelValue > 0"><icon-mdi-chevron-double-left /></button></div>
 
 		<div class="w-10"
 		><button
 			class="bg-white border rounded h-8 px-2"
 			@click="changePage(-1)" v-if="modelValue > 0"
-		>Left</button></div>
+		><icon-mdi-chevron-left /></button></div>
 
 		<div
-			class="flex-1 text-center"
-		>Page {{modelValue + 1}} of {{displayMaxPage}}</div>
+			class="flex-1 text-center h-8 pt-1 flex items-center justify-center"
+		><span v-if="length">{{displayShowingItems}} of {{length}}</span></div>
 
 		<div class="w-10"
 		><button
 			class="bg-white border rounded h-8 px-2"
 			@click="changePage(1)" v-if="modelValue < maxPage"
-		>Right</button></div>
+		><icon-mdi-chevron-right /></button></div>
 
-		<div class="w-10"><button class="bg-white border rounded h-8 px-2" @click="goToPage(Infinity)" v-if="modelValue < maxPage - 1">Last</button></div>
+		<div class="w-10"><button class="bg-white border rounded h-8 px-2" @click="goToPage(Infinity)" v-if="modelValue < maxPage"><icon-mdi-chevron-double-right /></button></div>
 	</div>
 </template>
 
@@ -42,12 +42,25 @@ const props = defineProps({
 })
 const emit = defineEmits(['update:modelValue']);
 
-const maxPage = computed(() => {
-	return Math.ceil(props.length / props.pageSize) - 1;
+const showingItems = computed(() => {
+	const start = props.pageSize * props.modelValue;
+	const end = Math.min(start + props.pageSize - 1, props.length - 1);
+	return [start, end];
 })
+const displayShowingItems = computed(() => {
+	if (props.length === 0) return [0, 0];
+	return `${showingItems.value[0] + 1}-${showingItems.value[1] + 1}`;
+})
+
+const numPages = computed(() => {
+	return Math.ceil(props.length / props.pageSize);
+})
+const maxPage = computed(() => {
+	return numPages.value - 1;
+})
+
 const displayMaxPage = computed(() => {
-	if (maxPage.value < 1) return 1;
-	return maxPage.value;
+	return Math.max(1, numPages.value);
 })
 
 const goToPage = (value) => {
