2c61ceded28dcebf0c567e85342015ea9f0bf539
Thilerion <michaelvanm@gmail.com>
2022-04-30 14:54:46 +0200

    Improved PuzzleRecap transition component

---
+++ src/components/puzzle-recap/PuzzleRecapBanner.vue
@@ -0,0 +1,133 @@
+<template>
+	<transition
+		name="t-banner"
+		@after-enter="afterEnter"
+		@after-leave="afterLeave"
+		:duration="{enter: enterDuration, leave: leaveDuration}"
+	>
+	<div
+		class="banner-container bg-black text-white w-full my-auto flex justify-center items-center relative py-1"
+		v-show="show"
+	>
+		<div
+			class="banner min-h-[2rem] px-4 py-6"
+			@click="emit('close')"
+		>
+			<div class="text-xl">
+				<slot>{{bannerText}}</slot>
+			</div>
+		</div>
+	</div>
+	</transition>
+</template>
+
+<script setup>
+import { pickRandom } from '@/lib/utils';
+import { ref, toRefs, computed } from 'vue';
+
+const BANNER_STRINGS = [
+	'Good job!',
+	'Sensational!',
+	'Wow!',
+	'Terrific!',
+	'Excellent!',
+	'Outstanding!',
+	'Sensational!',
+	'Exceptional!',
+	'Fantastic!',
+	'Marvelous!',
+	'Fabulous!',
+	'Tremendous job!',
+	'Superb!',
+	'Way to go!',
+	'Wonderful!',
+	"Amazing!",
+];
+
+const emit = defineEmits(['close', 'banner-after-enter', 'banner-after-leave']);
+const props = defineProps({
+	show: Boolean,
+	enterDuration: {
+		type: Number,
+		default: 400
+	},
+	leaveDuration: {
+		type: Number,
+		default: 200
+	}
+})
+
+const { enterDuration, leaveDuration } = toRefs(props);
+const enterDurMs = computed(() => enterDuration.value + 'ms');
+const leaveDurMs = computed(() => leaveDuration.value + 'ms');
+const bannerEnterDurMs = computed(() => {
+	const ms = Math.floor(enterDuration.value * 0.25);
+	return Math.max(ms, 100) + 'ms';
+})
+
+const afterEnter = () => {
+	emit('banner-after-enter');
+}
+const afterLeave = () => {
+	emit('banner-after-leave');
+}
+
+const bannerText = ref(
+	pickRandom(BANNER_STRINGS)
+);
+</script>
+
+<style scoped>
+.banner-container {
+	box-shadow: rgba(0, 0, 0, 0.35) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+}
+
+.t-banner-enter-active {
+	transform: translateX(-100%);
+  	animation: slideIn v-bind(enterDurMs) cubic-bezier(0.165, 0.84, 0.44, 1) forwards;
+}
+.t-banner-enter-active .banner {
+	opacity: 0;
+  	animation: fadeIn v-bind(enterDurMs) cubic-bezier(0.165, 0.84, 0.44, 1) forwards v-bind(bannerEnterDurMs);
+}
+.t-banner-enter-active .banner-container {
+	transition: box-shadow v-bind(enterDurMs) ease;
+}
+.t-banner-enter-from .banner-container {
+	box-shadow: rgba(0, 0, 0, 0) 0px 54px 55px, rgba(0, 0, 0, 0) 0px -12px 30px, rgba(0, 0, 0, 0) 0px 4px 6px, rgba(0, 0, 0, 0) 0px 12px 13px, rgba(0, 0, 0, 0) 0px -3px 5px;
+}
+
+.t-banner-leave-active {
+	transition: opacity v-bind(leaveDurMs) ease;
+}
+.t-banner-leave-to {
+	opacity: 0;
+}
+</style>
+
+<style>
+@keyframes slideIn {
+  0% {
+    transform: translateX(-100%);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+@keyframes fadeIn {
+	0% {
+		opacity: 0;		
+	}
+	100% {
+		opacity: 1;		
+	}
+}
+@keyframes fadeOut {
+	0% {
+		opacity: 0;
+	}
+	100% {
+		opacity: 1;
+	}
+}
+</style>
---
+++ src/components/puzzle-recap/PuzzleRecapTransitionWrapper.vue
@@ -0,0 +1,75 @@
+<template>
+	<transition
+		name="recap-outer"
+		:duration="{enter: bannerEnterDuration, leave: outerLeaveDuration}"
+		@after-enter="afterEnterOuter"
+		@after-leave="afterLeaveOuter"
+	>
+		
+	</transition>
+</template>
+
+<script setup>
+import { computed, ref } from 'vue';
+
+const outerEnterDuration = ref(400);
+const bannerEnterDuration = ref(500);
+const outerLeaveDuration = ref(200);
+</script>
+
+<style scoped>
+.recap-outer-enter-active {
+	transform: translateX(-100%);
+  	animation: slideIn v-bind(outerEnterDuration)ms cubic-bezier(0.165, 0.84, 0.44, 1) forwards;
+}
+
+.recap-outer-enter-active .banner-container {
+	transition: box-shadow v-bind(bannerEnterDuration)ms ease;
+}
+
+.recap-outer-enter-active .banner {
+	opacity: 0;
+  	animation: fadeInInner v-bind(bannerEnterDuration)ms cubic-bezier(0.165, 0.84, 0.44, 1) forwards;
+}
+
+.recap-outer-enter-from .banner-container {
+	box-shadow: rgba(0, 0, 0, 0) 0px 54px 55px, rgba(0, 0, 0, 0) 0px -12px 30px, rgba(0, 0, 0, 0) 0px 4px 6px, rgba(0, 0, 0, 0) 0px 12px 13px, rgba(0, 0, 0, 0) 0px -3px 5px;
+}
+
+.recap-outer-leave-active {
+	transition: opacity v-bind(outerLeaveDuration)ms ease;
+}
+.recap-outer-leave-to {
+	opacity: 0;
+}
+</style>
+
+<style>
+@keyframes slideIn {
+  0% {
+    transform: translateX(-100%);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+@keyframes fadeInInner {
+	0% {
+		opacity: 0;		
+	}
+	20% {
+		opacity: 0;
+	}
+	100% {
+		opacity: 1;		
+	}
+}
+@keyframes fadeOut {
+	0% {
+		opacity: 0;
+	}
+	100% {
+		opacity: 1;
+	}
+}
+</style>
