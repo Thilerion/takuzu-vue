f7add2c68abadee5c995e0e74efc47a5e654e099
Thilerion <michaelvanm@gmail.com>
2022-06-12 14:29:45 +0200

    Hint highlights and highlights store

---
+++ src/stores/highlight-store.js
@@ -0,0 +1,52 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const useHintHighlightsStore = defineStore('hintHighlights', () => {
+	const visible = ref(false);
+	const currentHighlights = ref([]);
+
+	const show = () => {
+		if (visible.value) return;
+		else if (currentHighlights.value?.length) {
+			console.warn(`Trying to show highlights, but none are set... Reverting highlight visibility to hidden.`);
+			visible.value = false;
+			return;
+		} else {
+			visible.value = true;
+		}
+	}
+	const hide = () => {
+		if (!visible.value) return;
+		visible.value = false;
+	}
+	const setHighlights = (highlights, { setVisible = false } = {}) => {
+		if (!Array.isArray(highlights)) {
+			console.warn('Setting highlights requires an array.');
+			return;
+		}
+
+		if (highlights.length) {
+			currentHighlights.value = [...highlights];
+			if (setVisible) {
+				visible.value = true;
+			}
+		} else {
+			currentHighlights.value = [];
+			if (visible.value) {
+				console.warn('Current highlights are unset, so will now automatically hide the highlights.');
+				visible.value = false;
+			}
+		}
+	}
+	const clear = () => {
+		visible.value = false;
+		currentHighlights.value = [];
+	}
+
+	return {
+		visible, currentHighlights,
+
+		show, hide, clear,
+		setHighlights
+	}
+});
