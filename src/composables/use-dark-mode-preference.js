dc5c3456225fab1f3a19c8e36b7592a53a82ce45
Thilerion <michaelvanm@gmail.com>
2022-06-20 16:00:00 +0200

    Set base theme/color pref in index.html

--- src/composables/use-dark-mode-preference.js
+++ src/composables/use-dark-mode-preference.js
@@ -1,17 +1,20 @@
 import { usePreferredColorScheme, useStorage } from "@vueuse/core";
 import { computed, inject, provide, readonly, watchEffect } from "vue";
 
+// set by script in index.html; this way the theme is immediately set
+const {
+	STORAGE_KEY = 'takuzu-theme',
+	setBaseThemeAttrs
+} = window.themePreferences ?? {};
+
 const VALID_PREFERENCES = ['auto', 'light', 'dark'];
 const VALID_COLOR_SCHEMES = ['light', 'dark'];
 const COLOR_MODE_INJECTION_KEY = Symbol('color-mode-inject');
-const STORAGE_KEY = 'takuzu-theme';
 const DEFAULT_SCHEME = 'light';
 
 export const useColorSchemeProvider = () => {
 	const userPreference = useStorage(STORAGE_KEY, DEFAULT_SCHEME);
 	const browserPreferred = usePreferredColorScheme();
-	
-	const htmlEl = document.querySelector('html');
 
 	const colorTheme = computed(() => {
 		if (userPreference.value === 'auto') {
@@ -23,22 +26,19 @@ export const useColorSchemeProvider = () => {
 		}
 	})
 
+	const isDark = computed(() => colorTheme.value === 'dark');
+	const isLight = computed(() => colorTheme.value === 'light');
+	const isAuto = computed(() => userPreference.value === 'auto');
+
 	watchEffect(() => {
 		console.log(`Color theme changed to: "${colorTheme.value}"`);
-		const enable = colorTheme.value;
-		const disable = enable === 'light' ? 'dark' : 'light';
-		htmlEl.classList.add(enable);
-		htmlEl.classList.remove(disable);
+		setBaseThemeAttrs(colorTheme.value);
 	})
 
 	const setColorTheme = (value) => {
 		console.log('Setting user preferred color theme.');
 		userPreference.value = value;
 	}
-
-	const isDark = computed(() => colorTheme.value === 'dark');
-	const isLight = computed(() => colorTheme.value === 'light');
-	const isAuto = computed(() => userPreference.value === 'auto');
 
 	const setToDefault = () => setColorTheme(DEFAULT_SCHEME);
 
