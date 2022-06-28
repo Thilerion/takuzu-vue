a39760ffa77df31a3ab2075babafc8f5de993c28
Thilerion <michaelvanm@gmail.com>
2022-06-28 13:47:47 +0200

    LineBalance strategy in typescript

--- src/lib/strategies/LineBalance.ts
+++ src/lib/strategies/LineBalance.ts
@@ -1,17 +1,20 @@
 import type { BoardLine } from "../board/BoardLine";
-import { ONE, ZERO } from "../constants";
+import { ONE, ZERO, type PuzzleSymbol } from "../constants";
 
-export function checkLineBalanceStrategy(boardLine: BoardLine) {
-	if (boardLine.isFilled) return { found: false };
+type LineBalanceNoResult = { found: false };
+type LineBalanceResult = { found: true, value: PuzzleSymbol };
+
+export function checkLineBalanceStrategy(boardLine: BoardLine): LineBalanceNoResult | LineBalanceResult {
+	if (boardLine.isFilled) return { found: false } as const;
 
 	const remainingOne = boardLine.getValueRemaining(ONE);
 	const remainingZero = boardLine.getValueRemaining(ZERO);
 
 	if (remainingZero === 0 && remainingOne > 0) {
-		return { found: true, value: ONE };
+		return { found: true, value: ONE } as const;
 	}
 	if (remainingOne === 0 && remainingZero > 0) {
-		return { found: true, value: ZERO };
+		return { found: true, value: ZERO } as const;
 	}
-	return { found: false };
+	return { found: false } as const;
 }
