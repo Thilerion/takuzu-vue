f48eccb6f7f902dbe59e2deb661275c1668fe1ec
Thilerion <michaelvanm@gmail.com>
2022-07-02 12:19:08 +0200

    Remove global components file (uses auto import now)

--- src/global-components.js
+++
@@ -1,9 +0,0 @@
-export function registerGlobalComponents(app) {
-	const globalComponents = import.meta.globEager('./components/global/**/*.vue');
-
-	Object.entries(globalComponents).forEach(([path, definition]) => {
-		const name = path.split('/').pop().replace(/\.\w+$/, '');
-		
-		app.component(name, definition.default);
-	})
-}
--- src/main.js
+++ src/main.js
@@ -3,15 +3,13 @@ import './fonts.css';
 import './tailwind.css';
 import App from './App.vue';
 
-import { registerGlobalComponents } from './global-components.js';
-
 import { registerSW } from 'virtual:pwa-register';
 
 import router from './router';
 import { createPinia } from 'pinia';
 
 const SW_UPDATE_INTERVAL_MS = 60 * 60 * 1000;
-const updateSW = registerSW({
+/* const updateSW =  */registerSW({
 	immediate: true,
 	onRegistered(r) {
 		r && setInterval(() => {
@@ -23,7 +21,5 @@ const updateSW = registerSW({
 const app = createApp(App)
 	.use(router)
 	.use(createPinia())
-
-registerGlobalComponents(app);
 	
 app.mount('#app')
