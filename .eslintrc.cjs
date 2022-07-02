96a2dea39c67e5b6a36d12fef633b6eef4b3e7d1
Thilerion <michaelvanm@gmail.com>
2022-07-02 12:03:54 +0200

    Improved eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,25 @@
+// @ts-check
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution');
 
-module.exports = {
+module.exports = /** @type {import("eslint").Linter.BaseConfig} */ ({
+	root: true,
+	env: {
+		'es2022': true,
+		'node': true
+	},
 	extends: [
 		'eslint:recommended',
 		'plugin:vue/vue3-essential',
 		'@vue/eslint-config-typescript/recommended'
-	]
-}
+	],
+	rules: {
+		"@typescript-eslint/ban-ts-comment": [1, {
+			'ts-expect-error': 'allow-with-description',
+			'ts-ignore': 'allow-with-description',
+			'ts-nocheck': 'allow-with-description',
+			'ts-check': false,
+			minimumDescriptionLength: 10
+		}]
+	},
+});
