933a7de92cfe3a77ad438b25ae54eeea4d2b203d
Thilerion <michaelvanm@gmail.com>
2022-07-02 18:50:52 +0200

    Include version of previously installed build in buildData

--- src/app.globals.ts
+++ src/app.globals.ts
@@ -1,24 +1,70 @@
-import { provide, inject } from 'vue';
+import { useStorage } from '@vueuse/core';
+import { provide, inject, computed, type ComputedRef } from 'vue';
 
 const key = Symbol('appGlobalBuildData');
+const lsKeyVersion = 'takuzu_app-versions';
 
-export interface GlobalBuildData {
+interface AppVersionDataPersisted {
+	version: string
+	previous: null | string;
+}
+
+interface AppVersionDataRefs {
+	curAppVersion: ComputedRef<AppVersionDataPersisted['version']>;
+	prevAppVersion: ComputedRef<AppVersionDataPersisted['previous']>;
+}
+
+interface GlobalDefinedBuildData {
 	pkgVersion: typeof __PKG_VERSION__;
 	buildDate: typeof __BUILD_DATE__;
 }
 
+const __PKG_VERSION__ = '0.14.1';
+
+export interface InjectedBuildData extends AppVersionDataRefs, GlobalDefinedBuildData { }
+
 export const provideGlobalBuildData = () => {
+	const appVersionData = useStorage<AppVersionDataPersisted>(lsKeyVersion, {
+		version: '',
+		previous: null
+	});
+	const prevAppVersion = computed(() => appVersionData.value.previous);
+	const curAppVersion = computed(() => appVersionData.value.version);
+
+	const constructedVersion = `${__PKG_VERSION__}_${__BUILD_DATE__}`;
+
+	if (constructedVersion !== appVersionData.value.version) {
+		const version = constructedVersion;
+		const storedVersion = appVersionData.value.version;
+		const hasStoredVersion = storedVersion != null && storedVersion !== '';
+		const previous = hasStoredVersion ? storedVersion : null;
+		appVersionData.value = { version, previous };
+
+		if (hasStoredVersion) {
+			console.log(`App version increased from "${storedVersion}" to "${version}"!`);
+			if (import.meta.env.DEV) {
+				window.alert(`App version increased from "${storedVersion}" to "${version}"!`)
+			}
+		} else {
+			console.log(`App version has been set for the first time to "${version}"!`);
+			if (import.meta.env.DEV) {
+				window.alert(`App version has been set for the first time to "${version}"!`)
+			}
+		}
+	}
 	provide(key, {
 		pkgVersion: __PKG_VERSION__,
+		curAppVersion,
+		prevAppVersion,
 		buildDate: __BUILD_DATE__
-	} as GlobalBuildData)
+	} as InjectedBuildData)
 }
 
-export const useGlobalBuildData = (): GlobalBuildData => {
+export const useGlobalBuildData = (): InjectedBuildData => {
 	const buildData = inject(key);
 	if (buildData === undefined) {
 		throw new Error('Injected build data must not be undefined.')
 	}
-	const { pkgVersion, buildDate } = (buildData as GlobalBuildData);
-	return { pkgVersion, buildDate };
+	const { pkgVersion, buildDate, curAppVersion, prevAppVersion } = (buildData as InjectedBuildData);
+	return { pkgVersion, buildDate, curAppVersion, prevAppVersion };
 }
--- src/views/SettingsPage.vue
+++ src/views/SettingsPage.vue
@@ -113,8 +113,23 @@
 					<button
 						@click="toggleDebugMode(true, { immediate: false })"
 						:disabled="isDebugModeEnabled"
+						class="w-full"
 					>
-					<span class="mr-4 text-base">App version:</span>{{ pkgVersion }}_<small>{{buildDate}}</small>
+						<div class="flex flex-row gap-x-2 items-start h-full text-start w-full max-w-[40ch] overflow-x-hidden">
+							<div class="min-w-fit h-full grow-[20] shrink-[1]">
+								<div class="text-start">App version</div>
+								<div class="text-xs text-start opacity-50" v-if="prevAppVersion && prevAppVersion.includes('_')">Previous version</div>
+							</div>
+							<div class="grow-[25] shrink-[2] h-full flex flex-col min-w-0">
+								<div class="text-ellipsis">{{ curAppVersionSplit.pkgVersion }}<small>_{{curAppVersionSplit.buildDate}}</small></div>
+								<div class="text-xs opacity-50 tracking-wider text-ellipsis" v-if="prevAppVersion && prevAppVersion.includes('_')">{{prevAppVersion}}</div>
+							</div>
+							<!-- <div class="flex-auto">{{ curAppVersionSplit.pkgVersion }}<small>{{curAppVersionSplit.buildDate}}</small></div> -->
+							<!-- <div class="">
+								<span class="mr-4 text-xs">{{ prevAppVersion }}</span>
+							</div> -->
+						</div>
+						
 					</button>
 				</BasicLinkListItem>
 				<BasicLinkListItem v-if="isDebugModeEnabled"><router-link to="/debug-options">Developer options</router-link></BasicLinkListItem>
@@ -136,7 +151,7 @@ import { useSettingsStore } from '@/stores/settings/store';
 import { storeToRefs } from 'pinia';
 import { useGlobalBuildData } from '@/app.globals';
 import { useThemePreferences } from '@/composables/use-theme-preferences';
-import { onBeforeUnmount } from 'vue';
+import { computed, onBeforeUnmount } from 'vue';
 import { CheckButtonOption, rulerType } from '@/stores/settings/options';
 
 defineProps({
@@ -175,7 +190,11 @@ const lineInfoOptions = [
 	{ label: 'Current values in line', value: rulerType.COUNT_CURRENT}
 ]
 
-const { pkgVersion, buildDate } = useGlobalBuildData();
+const { curAppVersion, prevAppVersion } = useGlobalBuildData();
+const curAppVersionSplit = computed(() => {
+	const [pkgVersion, buildDate] = curAppVersion.value.split('_');
+	return { pkgVersion, buildDate };
+})
 
 const closeSettings = () => {
 	const prev = this.$route.meta.prev;
