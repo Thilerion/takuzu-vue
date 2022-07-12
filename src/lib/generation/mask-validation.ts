35a5c997bc71b36659fa989a01a42b54690abfce
Thilerion <michaelvanm@gmail.com>
2022-07-12 13:16:56 +0200

    Crappy mask-validation typescript conversion

--- src/lib/generation/mask-validation.ts
+++ src/lib/generation/mask-validation.ts
@@ -1,18 +1,30 @@
-import { applyEliminationConstraint, applyLineBalanceConstraint, applyTriplesConstraint } from "@/lib/solver/constraints";
+import { applyEliminationConstraint, applyLineBalanceConstraint, applyTriplesConstraint, type ConstraintFn } from "@/lib/solver/constraints";
 import Solver from "@/lib/solver/Solver";
+import type { SimpleBoard } from "../board/Board";
+import type { BasicPuzzleConfig, DifficultyKey } from "../types";
 
-const baseCanSolveWith = ({constraintFns, disableBacktracking = true} = {}) => (maskedBoard) => {
+// TODO: simplify these types and functions
+
+type BaseSolveCheckOpts = {
+	disableBacktracking?: boolean,
+	constraintFns: DifficultyConstraintFnValue
+}
+type BaseCannotSolveCheckOpts = {
+	disableBacktracking?: boolean,
+	constraintFns: DifficultyConstraintFnValueOrNull
+}
+const baseCanSolveWith = ({constraintFns, disableBacktracking = true}: BaseSolveCheckOpts) => (maskedBoard: SimpleBoard) => {
 	const solutions = Solver.run(maskedBoard, {
 		maxSolutions: 2,
 		timeoutDuration: 2000,
 		throwAfterTimeout: false,
 		disableBacktracking,
-		constraintFns
+		constraintFns: constraintFns as ConstraintFn[]
 	})
 	return solutions?.length === 1;
 }
 
-const baseCannotSolveWith = ({constraintFns, disableBacktracking = true} = {}) => (maskedBoard) => {
+const baseCannotSolveWith = ({constraintFns, disableBacktracking = true}: BaseCannotSolveCheckOpts) => (maskedBoard: SimpleBoard) => {
 	if (!Array.isArray(constraintFns)) {
 		return true;
 	}
@@ -21,7 +33,7 @@ const baseCannotSolveWith = ({constraintFns, disableBacktracking = true} = {}) =
 		timeoutDuration: 2000,
 		throwAfterTimeout: false,
 		disableBacktracking,
-		constraintFns
+		constraintFns: constraintFns as ConstraintFn[]
 	})
 	return solutions?.length === 0;
 }
@@ -29,40 +41,54 @@ const baseCannotSolveWith = ({constraintFns, disableBacktracking = true} = {}) =
 export const baseConstraintFns = {
 	TRIPLES: applyTriplesConstraint,
 	BALANCE: applyLineBalanceConstraint,
-	ELIM_1: (board) => applyEliminationConstraint(board, {
+	ELIM_1: (board: SimpleBoard) => applyEliminationConstraint(board, {
 		singleAction: false,
 		maxLeast: 1,
 		minLeast: 1,
 		enforceUniqueLines: false,
 	}),
-	ELIM_2: (board) => applyEliminationConstraint(board, {
+	ELIM_2: (board: SimpleBoard) => applyEliminationConstraint(board, {
 		singleAction: false,
 		maxLeast: 2,
 		minLeast: 2,
 		enforceUniqueLines: false,
 	}),
-	ELIM_INF: (board) => applyEliminationConstraint(board, {
+	ELIM_INF: (board: SimpleBoard) => applyEliminationConstraint(board, {
 		singleAction: false,
 		maxLeast: Infinity,
 		minLeast: 3,
 		enforceUniqueLines: false
 	}),
-	DUPE_ELIM_1: (board) => applyEliminationConstraint(board, {
+	DUPE_ELIM_1: (board: SimpleBoard) => applyEliminationConstraint(board, {
 		singleAction: false,
 		maxLeast: 1,
 		minLeast: 1,
 		enforceUniqueLines: true
 	}),
-	DUPE_ELIM_2: (board) => applyEliminationConstraint(board, {
+	DUPE_ELIM_2: (board: SimpleBoard) => applyEliminationConstraint(board, {
 		singleAction: false,
 		maxLeast: 2,
 		minLeast: 2,
 		enforceUniqueLines: true
 	}),
 	BACKTRACKING: 'BACKTRACKING'
-}
+} as const;
 
-const defaultDifficultyConstraintFns = {
+type BaseValidateConstraintFnMap = typeof baseConstraintFns;
+type BaseValidateConstraintFnTypes = keyof BaseValidateConstraintFnMap;
+type BaseValidateConstraintFnValues = BaseValidateConstraintFnMap[BaseValidateConstraintFnTypes];
+type DifficultyConstraintFnValue = BaseValidateConstraintFnValues[];
+type DifficultyConstraintFnValueOrNull = null | DifficultyConstraintFnValue;
+
+type DefaultDifficultyConstraintFns = Record<
+	DifficultyKey,
+	({
+		canNotSolveWithConstraints: DifficultyConstraintFnValueOrNull,
+		canSolveWithConstraints: DifficultyConstraintFnValue
+	})
+>
+
+const defaultDifficultyConstraintFns: DefaultDifficultyConstraintFns = {
 	1: {
 		canNotSolveWithConstraints: null,
 		canSolveWithConstraints: [
@@ -129,15 +155,18 @@ const defaultDifficultyConstraintFns = {
 		]
 	}
 }
-
-const overrideDifficultyConstraintFns = {
+type MaskValidator = {
+	canSolveWith: ReturnType<typeof baseCanSolveWith>,
+	canNotSolveWith: ReturnType<typeof baseCannotSolveWith>
+}
+const overrideDifficultyConstraintFns: Record<string, MaskValidator> = {
 	'6x6-3': {
 		canSolveWith: baseCanSolveWith({ constraintFns: defaultDifficultyConstraintFns[4].canSolveWithConstraints}),
 		canNotSolveWith: baseCannotSolveWith({ constraintFns: defaultDifficultyConstraintFns[4].canNotSolveWithConstraints})
 	},
 }
 
-export const getMaskValidatorsForPuzzleConfig = ({ width, height, difficulty }) => {
+export const getMaskValidatorsForPuzzleConfig = ({ width, height, difficulty }: BasicPuzzleConfig): MaskValidator => {
 	const overrideKey = `${width}x${height}-${difficulty}`;
 
 	if (overrideDifficultyConstraintFns[overrideKey] != null) {
