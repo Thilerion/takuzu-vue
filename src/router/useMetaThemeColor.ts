bed31f66150416932067398f92b2c7797d940823
Thilerion <michaelvanm@gmail.com>
2022-08-05 15:58:37 +0200

    Improve useMetaThemeColor types

--- src/router/useMetaThemeColor.ts
+++ src/router/useMetaThemeColor.ts
@@ -1,35 +1,41 @@
-import { readonly, ref } from "vue";
-import type { RouteLocationNormalized } from "vue-router";
+import { readonly, ref, type Ref } from "vue";
+import type { RouteLocationNormalized } from 'vue-router';
+
+export type UseMetaThemeColorOpts = {
+	defaultColor?: string;
+};
 
 export const useMetaThemeColor = ({
 	defaultColor = 'white'
-} = {}) => {
+}: UseMetaThemeColorOpts = {}) => {
 	// TODO: different colors for dark/light mode
-	const metaEl = document.querySelector("meta[name=theme-color]");
-	const getCurrentThemeColor = () => metaEl!.getAttribute("content");
-	
-	const color = ref(getCurrentThemeColor());
+	const metaEl = document.querySelector('meta[name=theme-color]');
 
+	const getCurrentThemeColor = () => {
+	  return metaEl!.getAttribute('content') ?? null;
+	};
+  
+	const initialColor = getCurrentThemeColor() ?? defaultColor;
+	const color: Ref<string> = ref(initialColor);
+  
 	const setCurrentThemeColor = (value: string) => {
-		metaEl!.setAttribute("content", value);
-		color.value = value;
+	  metaEl!.setAttribute('content', value);
+	  color.value = value;
+	};
+  
+	if (getCurrentThemeColor() == null) {
+	  setCurrentThemeColor(color.value);
 	}
 
 	return {
 		color: readonly(color),
 		setThemeColor: setCurrentThemeColor,
 		updateThemeColorWithRouteMeta: (to: RouteLocationNormalized) => {
-			const newColor = to?.meta?.metaThemeColor ?? defaultColor;
-			if (!newColor) {
-				return;
-			} else if (typeof newColor !== 'string') {
-				console.warn('Meta theme-color should be a string.');
-				console.warn({ newColor });
-				return;
-			}
-			if (newColor !== color.value) {
-				setCurrentThemeColor(newColor);
-			}
-		}
-	}
+		  const newColor = to.meta?.metaThemeColor;
+		  if (newColor == null) return;
+		  if (newColor !== color.value) {
+			setCurrentThemeColor(newColor);
+		  }
+		},
+	  };
 }
