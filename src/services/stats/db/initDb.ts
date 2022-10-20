34006c57145b85aaeaa865b4f2c28f177eaa3bf9
Michael van Meerwijk <michaelvanm@gmail.com>
2022-10-20 15:21:35 +0200

    Stats DB helper methods

--- src/services/stats/db/initDb.ts
+++ src/services/stats/db/initDb.ts
@@ -13,6 +13,41 @@ class StatsDb extends Dexie {
 		this.puzzleHistory.mapToClass(DbHistoryEntry);
 		persistStorage({ preventPermissionPrompt: true });
 	}
+
+	clearHistory() {
+		return this.puzzleHistory.clear();
+	}
+	async addHistoryItem(item: DbHistoryEntry, updateItemId = true) {
+		const primaryKey = await this.puzzleHistory.add(item);
+		if (updateItemId) item.id = primaryKey;
+		return primaryKey;
+	}
+	async updateHistoryItem(primaryKey: number, modifiedProps: Partial<DbHistoryEntry> = {}) {
+		const success = await this.puzzleHistory.update(primaryKey, modifiedProps);
+		if (!success) {
+			console.warn(`Updating item with primary key "${primaryKey}" failed; probably because no item could be found with this key.`);
+		}
+		return !!success;
+	}
+	putHistoryItem(item: DbHistoryEntry) {
+		if (item == null) {
+			throw new Error('Invalid item to put in db.');
+		}
+		if (item?.id == null) {
+			throw new Error('When putting item in database, it should have a primary key (id). Otherwise, use "add".');
+		}
+		return this.puzzleHistory.put(item);
+	}
+	deleteHistoryItem(primaryKey: number) {
+		return this.puzzleHistory.delete(primaryKey);
+	}
+
+	getCount(table: Parameters<typeof this.table>[0] = 'puzzleHistory') {
+		return this.table(table).count();
+	}
+	getAll(table: Parameters<typeof this.table>[0] = 'puzzleHistory') {
+		return this.table(table).toArray();
+	}
 }
 
 export const db = new StatsDb();
