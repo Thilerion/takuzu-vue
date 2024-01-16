0d267af3632f984e00f5217f76898197a10029ff
Thilerion <michaelvanm@gmail.com>
2024-01-16 09:04:50 +0100

    Fix options.ts const types

--- src/stores/settings/options.ts
+++ src/stores/settings/options.ts
@@ -1,24 +1,24 @@
 import type { CellTheme, CellThemeType, RulerType } from './types';
 
 export { validVibrationStrengths, cellThemeTypeMap as CellThemeTypeMap } from './types';
-export const rulerType: Record<string, RulerType> = {
+export const rulerType = {
 	NONE: '',
 	COORDS: 'coords',
 	COUNT_REMAINING: 'remainingCount',
 	COUNT_CURRENT: 'currentCount'
-} as const;
+} as const satisfies Record<string, RulerType>;
 
-export const CellThemeTypes: Record<string, CellThemeType> = {
+export const CellThemeTypes = {
 	COLORED_TILES: 'coloredTiles',
 	SYMBOLS: 'symbols'
-} as const;
-export const CellThemes: Record<string, CellTheme> = {
+} as const satisfies Record<string, CellThemeType>;
+export const CellThemes = {
 	CLASSIC: 'classic',
 	TICTACTOE: 'tictactoe',
 	BLUE_RED: 'blue-red'
-} as const;
+} as const satisfies Record<string, CellTheme>;
 export const CheckButtonOption = {
 	DISABLED: 'disabled',
 	RULE_VIOLATIONS: 'ruleViolations',
 	INCORRECT_VALUES: 'incorrectValues'
-};
+} as const;
