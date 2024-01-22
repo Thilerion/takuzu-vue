e3ff85d1a5e0499addef485d72fdb341abc4d1db
Thilerion <michaelvanm@gmail.com>
2024-01-22 14:43:47 +0100

    Improve puzzle-hinter store, and add module for hint search

---
+++ src/stores/hints/search.ts
@@ -0,0 +1,104 @@
+import { SimpleBoard } from "@/lib";
+import { createHint } from "./helpers";
+import { HINT_TYPE, Hint } from "./Hint";
+import { humanSolveBalance, humanSolveDuplicateLine, humanSolveElimination, humanSolveTriples } from "@/lib/human-solver";
+
+export const searchForHint = (
+	board: SimpleBoard,
+	solution: SimpleBoard,
+): Hint | null => {
+
+	// Step 1: check for incorrect values
+	const mistakeHint = searchForMistakesHint(board, solution);
+	if (mistakeHint) return mistakeHint;
+
+	// Step 2: run HumanSolver to find possible moves to make right now.
+	// Start with the easiest strategy and return early.
+	const humanStrategyHint = searchForHumanStrategyHint(board);
+	if (humanStrategyHint) return humanStrategyHint;
+
+	// Step 3: TODO: more advanced strategies, or random value from solution, or backtracking search
+	console.warn("SHOULD RUN DEEPER SEARCH FOR HINTS NOW, BUT NOT IMPLEMENTED YET.");
+	return null;
+}
+
+function searchForMistakesHint(board: SimpleBoard, solution: SimpleBoard): Hint | null {
+	// TODO: better to compare incorrect values AND rule violations, but for now, just check incorrect values
+		// 1: check if there are incorrect values
+		// 1a: if yes, check if there are rule violations
+		// 1b: is all incorrect values are due to rule violations, show ruleViolation hint
+		// 1c: if not all incorrect due to RV, are no RV at all, show IncorrectValue hint
+	const {
+		hasMistakes,
+		result: incorrectValues
+	} = board.hasIncorrectValues(solution);
+
+	if (hasMistakes) {
+		const hint = createHint(HINT_TYPE.MISTAKE, incorrectValues);
+		return hint;
+	}
+	return null;
+}
+
+function searchForHumanStrategyHint(board: SimpleBoard) {
+	// TRIPLES STRATEGY
+	const triplesHumanResult = humanSolveTriples({ board });
+	if (triplesHumanResult && triplesHumanResult.length) {
+		const triplesHints = triplesHumanResult.map(triplesResult => {
+			return createHint(HINT_TYPE.TRIPLES, triplesResult);
+		})
+		
+		const sortedHints = triplesHints.sort((a, b) => {
+			const { subType: typeA } = a;
+			const { subType: typeB } = b;
+			if (typeA === 'double' && typeB === 'sandwich') return -1;
+			if (typeA === 'sandwich' && typeB === 'double') return 1;
+			return b.targets.length - a.targets.length;
+		})
+		const hint = sortedHints[0];
+		return hint;
+	}
+	// BALANCE HINT
+	const balanceHintResult = humanSolveBalance({ board });
+	if (balanceHintResult && balanceHintResult.length) {
+		const hint = createHint(HINT_TYPE.BALANCE, balanceHintResult[0]);
+		return hint;
+	}
+	// ELIMINATION HINT
+	const eliminationHintResult = humanSolveElimination({ board });
+	if (!Array.isArray(eliminationHintResult)) {
+		const err = eliminationHintResult.error;
+		throw new Error(`Elimination Technique returned an error, but the mistakes checker found none. This should not be possible. Error: ${err}`);
+	}
+	if (eliminationHintResult.length) {
+		const sorted = [...eliminationHintResult].sort((a, b) => {
+			if (a.elimType === b.elimType) {
+				return 0;
+			} else if (a.elimType > b.elimType) {
+				return 1;
+			} else return -1;
+		})
+		const hint = createHint(HINT_TYPE.ELIMINATION, sorted[0]);
+		return hint;
+	}
+
+	// ELIMINATION/DUPE HINT
+	const dupeHintResult = humanSolveDuplicateLine({ board });
+	if (!Array.isArray(dupeHintResult)) {
+		const err = dupeHintResult.error;
+		throw new Error(`DuplicateLine Technique returned an error, but the mistakes checker found none. This should not be possible. Error: ${err}`);
+	}
+	if (dupeHintResult.length) {
+		const sorted = [...dupeHintResult].sort((a, b) => {
+			if (a.elimType === b.elimType) {
+				return 0;
+			} else if (a.elimType > b.elimType) {
+				return 1;
+			} else return -1;
+		})
+		const hint = createHint(HINT_TYPE.ELIM_DUPE, sorted[0]);
+		return hint;
+	}
+
+	return null;
+}
--- src/stores/puzzle-hinter.ts
+++ src/stores/puzzle-hinter.ts
@@ -1,11 +1,10 @@
-import { humanSolveBalance, humanSolveDuplicateLine, humanSolveElimination, humanSolveTriples } from "@/lib/human-solver";
-import { Hint, HINT_TYPE } from "./hints/Hint.js";
-import { createHint, validateHint } from "./hints/index.js";
+import { Hint } from "./hints/Hint.js";
+import { validateHint } from "./hints/index.js";
 import { defineStore } from "pinia";
 import { usePuzzleStore } from "./puzzle.js";
 import { useHintHighlightsStore } from "./highlight-store.js";
 import type { BoardString } from "@/lib/types";
-import type { SimpleBoard } from "@/lib/index";
+import { searchForHint } from "./hints/search";
 
 export const usePuzzleHintsStore = defineStore('puzzleHints', {
 
@@ -54,7 +53,7 @@ export const usePuzzleHintsStore = defineStore('puzzleHints', {
 		},
 		showNewHint(hint: Hint) {
 			this.currentHint = hint;
-			this.currentHintValidForBoardStr = this._getBoardStr() ?? null;
+			this.currentHintValidForBoardStr = usePuzzleStore().boardStr! ?? null;
 			this.showHint = true;
 			const hintHighlightsStore = useHintHighlightsStore();
 			hintHighlightsStore.displayFromHint(hint);
@@ -71,150 +70,77 @@ export const usePuzzleHintsStore = defineStore('puzzleHints', {
 		getHint() {
 			const puzzleStore = usePuzzleStore();
 			const boardStr = puzzleStore.boardStr!;
-			const cacheResult = this.cache.get(boardStr);
-			if (cacheResult && this.currentHint != null && this.currentHintValidForBoardStr === this._getBoardStr()) {
-				this.show();
-				return;
-			} else if (cacheResult) {
-				this.showNewHint(cacheResult);
+
+			// First, check if there is a cached hint for this board state
+			// If so, show it, to prevent undoing and redoing from showing a different hint
+			if (this.showCachedHintIfValid(boardStr)) {
 				return;
 			}
+
+			// Else, check if there is an active currentHint set.
+			// If so, check if it is still valid for the current board state. If not, removeHint and continue.
 			const board = puzzleStore.board!;
 			const solution = puzzleStore.solution!;
+			if (this.validateCurrentHint()) {
+				this.show();
+				return;
+			}
+
+			const hint = searchForHint(board, solution);
+			this.setHint(hint);
+		},
+
+		validateCurrentHint(): boolean {
 			const currentHint = this.currentHint;
-			if (currentHint) {
-				const isValid = validateHint(currentHint);
-				if (isValid) {
-					console.log('Hint is still valid. Showing it now.');
-					this.show();
-					return;
-				} else {
-					console.log('Hint is no longer valid. Will remove it and generate a new hint.');
-					this.removeHint();
-				}
-			}
+			if (!currentHint) return false;
 
-				// 1: check if there are incorrect values
-			// 1a: if yes, check if there are rule violations
-			// 1b: is all incorrect values are due to rule violations, show ruleViolation hint
-			// 1c: if not all incorrect due to RV, are no RV at all, show IncorrectValue hint
-
-			// TODO: would be best to compare incorrect values and rule violatoins, but for now, just check incorrect vlaues
-
-			const {
-				hasMistakes,
-				result: incorrectValues
-			} = board.hasIncorrectValues(solution);
-			if (hasMistakes) {
-				const hint = createHint(HINT_TYPE.MISTAKE, incorrectValues);
-				this.setHints([hint]);
-				return;
-			}
-
-			// 2: Run HumanSolver to find all possible moves to make right now (probably only the easiest strategy)
-			// findAllAvailableMoves({ board, solution });
-
-			// TRIPLES HINT
-			const triplesHumanResult = humanSolveTriples({ board });
-			if (triplesHumanResult && triplesHumanResult.length) {
-				const triplesHints = triplesHumanResult.map(triplesResult => {
-					return createHint(HINT_TYPE.TRIPLES, triplesResult);
-				})
-				
-				const sortedHints = triplesHints.sort((a, b) => {
-					const { subType: typeA } = a;
-					const { subType: typeB } = b;
-					if (typeA === 'double' && typeB === 'sandwich') return -1;
-					if (typeA === 'sandwich' && typeB === 'double') return 1;
-					return b.targets.length - a.targets.length;
-				})
-				const hint = sortedHints[0];
-				this.setHints([hint]);
-				return;
-			}
-
-			// BALANCE HINT
-			const balanceHintResult = humanSolveBalance({ board });
-			if (balanceHintResult && balanceHintResult.length) {
-				const hint = createHint(HINT_TYPE.BALANCE, balanceHintResult[0]);
-				this.setHints([hint]);
-				return;
-			}
-
-			// ELIMINATION HINT
-			const eliminationHintResult = humanSolveElimination({ board });
-			if (!Array.isArray(eliminationHintResult)) {
-				const err = eliminationHintResult.error;
-				throw new Error(`Elimination Technique returned an error, but the mistakes checker found none. This should not be possible. Error: ${err}`);
-			}
-			if (eliminationHintResult.length) {
-				const sorted = [...eliminationHintResult].sort((a, b) => {
-					if (a.elimType === b.elimType) {
-						return 0;
-					} else if (a.elimType > b.elimType) {
-						return 1;
-					} else return -1;
-				})
-				const hint = createHint(HINT_TYPE.ELIMINATION, sorted[0]);
-				this.setHints([hint]);
-				return;
-			}
-
-			// ELIMINATION/DUPE HINT
-			const dupeHintResult = humanSolveDuplicateLine({ board });
-			if (!Array.isArray(dupeHintResult)) {
-				const err = dupeHintResult.error;
-				throw new Error(`DuplicateLine Technique returned an error, but the mistakes checker found none. This should not be possible. Error: ${err}`);
-			}
-			if (dupeHintResult.length) {
-				const sorted = [...dupeHintResult].sort((a, b) => {
-					if (a.elimType === b.elimType) {
-						return 0;
-					} else if (a.elimType > b.elimType) {
-						return 1;
-					} else return -1;
-				})
-				const hint = createHint(HINT_TYPE.ELIM_DUPE, sorted[0]);
-				this.setHints([hint]);
-				return;
-			}
-
-			console.warn('SHOULD RUN HUMAN SOLVER NOW, BUT IS NOT YET IMPLEMENTED');
-
-
-			// TODO: not yet implemented hinting system
-			this.setHints();
-		},
-
-		setHints(hints: Hint[] = []) {
-			console.log('setting hints');
-			console.log({ hints });
-			// TODO: pick one single hint from a list of hints
-			if (hints.length === 0) {
-				console.warn('SetHints was called, but there were no hints in the array. Seems like there are no valid hints for this board state.');
-				this.removeHint();
-				// still add to cache, because no hint being available is also worth caching
-				this.addNoHintAvailableToCache();
+			const isValid = validateHint(currentHint);
+			if (isValid) {
+				console.log('Hint is still valid. Showing it now.');
+				return true;
 			} else {
-				const hint = hints[0];
-				this.showNewHint(hint);
-				this.addHintToCache({ hint });
-				return;
+				console.log('Hint is no longer valid. Will remove it and generate a new hint.');
+				this.removeHint();
+				return false;
+			}
+		},
+		showCachedHintIfValid(boardStr: BoardString): boolean {
+			const cacheResult = this.cache.get(boardStr);
+			if (!cacheResult) {
+				return false;
+			}
+			if (this.currentHint != null && this.currentHintValidForBoardStr === boardStr) {
+				this.show();
+				return true;
+			} else {
+				this.showNewHint(cacheResult);
+				return true;
 			}
 		},
 
-		_getBoardStr(): BoardString {
-			return usePuzzleStore().boardStr!;
+		setHint(hint: Hint | null) {
+			if (hint == null) {
+				console.log('SetHint was called without a hint; seems like there are no valid hints (found) for this board state.');
+				this.removeHint();
+				this.addNoHintAvailableToCache();
+				return;
+			}
+
+			console.log('setting hint');
+			console.log({ hint });
+
+			this.showNewHint(hint);
+			this.addHintToCache({ hint });
 		},
 
 		addHintToCache({
 			hint, boardStr
 		}: { hint: Hint, boardStr?: BoardString }) {
-			const _boardStr: BoardString = boardStr ?? this._getBoardStr();
+			const _boardStr: BoardString = boardStr ?? usePuzzleStore().boardStr!;
 			this.cache.set(_boardStr, hint);
 		},
 		addNoHintAvailableToCache(boardStr?: BoardString) {
-			this.cache.set(boardStr ?? this._getBoardStr(), null);
+			this.cache.set(boardStr ?? usePuzzleStore().boardStr!, null);
 		}
 	}
 
