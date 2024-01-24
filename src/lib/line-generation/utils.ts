8d93db74411ec526150c083e6d0e76962f0fec67
Thilerion <michaelvanm@gmail.com>
2024-01-24 11:07:57 +0100

    Moved permute function to line-gen/utils

---
+++ src/lib/line-generation/utils.ts
@@ -0,0 +1,32 @@
+export function permuteUnique<T>(arr: T[]) {
+	const perms: T[][] = [];
+	_permuteRemaining(arr, perms);
+	return perms;
+}
+
+function _permuteRemaining(arr: unknown[], perms: unknown[][], index = 0, n = arr.length) {
+	if (index >= n) {
+		perms.push([...arr]);
+		return;
+	}
+
+	for (let i = index; i < n; i++) {
+		if (_shouldSwap(arr, index, i)) {
+			_swap(arr, index, i);
+			_permuteRemaining(arr, perms, index + 1, n);
+			_swap(arr, index, i);
+		}
+	}
+}
+
+function _shouldSwap(arr: unknown[], start: number, cur: number) {
+	for (let i = start; i < cur; i++) {
+		if (arr[i] === arr[cur]) return false;
+	}
+	return true;
+}
+function _swap(arr: unknown[], idxA: number, idxB: number) {
+	const temp = arr[idxA];
+	arr[idxA] = arr[idxB];
+	arr[idxB] = temp;
+}
