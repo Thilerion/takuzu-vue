540b056318ceaf8a90f02a44bc99bb2ad7e1e7d2
Thilerion <michaelvanm@gmail.com>
2024-02-07 12:09:41 +0100

    Fix goBackOrReplaceTo with router historyState.back

--- src/router/useGoBackOrReplaceTo.ts
+++ src/router/useGoBackOrReplaceTo.ts
@@ -7,8 +7,27 @@ export const useGoBackOrReplaceTo = (to: RouteLocationRaw) => {
 }
 
 const goBackOrReplaceTo = (to: RouteLocationRaw, router: Router, route: RouteLocationNormalizedLoaded) => {
+	/* if (route.meta?.prev == null) {
+		router.replace(to);
+	} else {
+		const resolvedTo = router.resolve(to);
+		const historyState = router.options.history.state;
+		if (historyState.back != null && historyState.back === resolvedTo.fullPath) {
+			router.go(-1);
+		} else {
+			router.replace(to);
+		}
+	} */
 	if (route.meta?.prev == null) {
 		router.replace(to);
+	} else {
+		router.go(-1);
+	}
+}
+
+export const tryGoBack = (fallbackTo: string | { name: string }, router: Router, route: RouteLocationNormalizedLoaded) => {
+	if (route.meta?.prev == null) {
+		router.replace(fallbackTo);
 	} else {
 		router.go(-1);
 	}
