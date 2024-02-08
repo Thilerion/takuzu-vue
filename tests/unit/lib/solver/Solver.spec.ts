3fec019d38bbcee7f26d3524f825818a6dec0a98
Thilerion <michaelvanm@gmail.com>
2024-02-08 14:23:42 +0100

    More old Solver tests for solving with constraints only

--- tests/unit/lib/solver/Solver.spec.ts
+++ tests/unit/lib/solver/Solver.spec.ts
@@ -1,5 +1,9 @@
 import { SimpleBoard, Solver, selectCell, selectValue } from "@/lib/index.js";
+import applyEliminationConstraint from "@/lib/solver/constraints/Elimination.js";
+import applyLineBalanceConstraint from "@/lib/solver/constraints/LineBalance.js";
+import applyTriplesConstraint from "@/lib/solver/constraints/Triples.js";
 import type { SolverConfig } from "@/lib/solver/types.js";
+import type { BoardExportString } from "@/lib/types.js";
 
 describe("Solver", () => {
 	describe("using backtracking only", () => {
@@ -71,6 +75,127 @@ describe("Solver", () => {
 				maxSolutions: 2
 			}
 			expect(Solver.run(board, conf).length).toBe(2);
+		})
+	})
+
+	describe('without backtracking, with specific constraints', () => {
+		const getSolverConf = (
+			constraintFns: SolverConfig['constraintFns']
+		): SolverConfig => ({
+			disableBacktracking: true,
+			selectCell: selectCell.first_empty,
+			selectValue: selectValue.least_constraining,
+			constraintFns,
+			maxSolutions: 2,
+			throwAfterTimeout: false,
+			timeoutDuration: 5000
+		})
+
+		test('should solve a puzzle with triples only', () => {
+			const board = SimpleBoard.fromArrayOfLines([
+				'0.11.1',
+				'0....1',
+				'..11..',
+				'11..10',
+				'.1.1..',
+				'1..0.0'
+			]); // this board can be solved using just triples strat: pairs/doubles and sandwiches
+			const conf = getSolverConf([applyTriplesConstraint]);
+			const result = Solver.run(board, conf);
+			expect(result).toHaveLength(1);
+			expect(result[0].toBoardString()).toBe([
+				'001101',
+				'010011',
+				'101100',
+				'110010',
+				'010101',
+				'101010'
+			].join(''))
+		})
+
+		test('should solve a puzzle with line balance only', () => {
+			const board = SimpleBoard.fromArrayOfLines([
+				'0.00..',
+				'..11.1',
+				'..0010',
+				'.0..00',
+				'0.0.01',
+				'.0101.'
+			]) // board can be solved using just line balance strat
+			const conf = getSolverConf([applyLineBalanceConstraint]);
+			const result = Solver.run(board, conf);
+			expect(result).toHaveLength(1);
+			expect(result[0].toBoardString()).toBe([
+				'010011',
+				'001101',
+				'110010',
+				'101100',
+				'010101',
+				'101010'
+			].join(''))
+		})
+
+		test('a board that requires the triples technique can not be solved with just line balance', () => {
+			const boardRequiresTriples = SimpleBoard.fromArrayOfLines([
+				'0.11.1',
+				'0....1',
+				'..11..',
+				'11..10',
+				'.1.1..',
+				'1..0.0'
+			]);
+
+			const confBalanceOnly = getSolverConf([applyLineBalanceConstraint]);
+			const confTriplesOnly = getSolverConf([applyTriplesConstraint]);
+			const confBoth = getSolverConf([applyTriplesConstraint, applyLineBalanceConstraint]);
+
+			// can be solved with triples technique
+			expect(Solver.run(boardRequiresTriples, confTriplesOnly)).toHaveLength(1);
+			// cannot be solved with (just) balance technique
+			expect(Solver.run(boardRequiresTriples, confBalanceOnly)).toHaveLength(0);
+			// but can be solved with both again
+			expect(Solver.run(boardRequiresTriples, confBoth)).toHaveLength(1);
+		})
+
+		test('a board that requires line balance can not be solved with just triples', () => {
+			const boardRequiresBalance = SimpleBoard.fromArrayOfLines([
+				'0.00..',
+				'..11.1',
+				'..0010',
+				'.0..00',
+				'0.0.01',
+				'.0101.'
+			])
+			const confBalanceOnly = getSolverConf([applyLineBalanceConstraint]);
+			const confTriplesOnly = getSolverConf([applyTriplesConstraint]);
+			const confBoth = getSolverConf([applyTriplesConstraint, applyLineBalanceConstraint]);
+
+			// can be solved with balance technique
+			expect(Solver.run(boardRequiresBalance, confBalanceOnly)).toHaveLength(1);
+			// cannot be solved with (just) triples technique
+			expect(Solver.run(boardRequiresBalance, confTriplesOnly)).toHaveLength(0);
+			// but can be solved with both again
+			expect(Solver.run(boardRequiresBalance, confBoth)).toHaveLength(1);
+		})
+
+		test('a board that requires difficulty techniques cannot be solved with just triples or balance', () => {
+			// these boards require an elim-1-2 technique to be solved
+			const boardOdd = SimpleBoard.import('7x7;.1...............0..01......0...0..101....0.....0' as BoardExportString);
+			const boardRect = SimpleBoard.import('6x10;.1....01...1101010100101011010.1001.10.10.0....10....01..0.1' as BoardExportString);
+
+			const confEasy = getSolverConf([applyTriplesConstraint, applyLineBalanceConstraint]);
+			const confWithElim12 = getSolverConf([
+				applyTriplesConstraint, applyLineBalanceConstraint,
+				(board: SimpleBoard) => applyEliminationConstraint(board, { minLeast: 1, maxLeast: 2 })
+			])
+
+			// can be solved with elim-1-2 technique
+			expect(Solver.run(boardOdd, confWithElim12)).toHaveLength(1);
+			expect(Solver.run(boardRect, confWithElim12)).toHaveLength(1);
+
+			// cannot be solved with (just) triples or balance techniques
+			expect(Solver.run(boardOdd, confEasy)).toHaveLength(0);
+			expect(Solver.run(boardRect, confEasy)).toHaveLength(0);
 		})
 	})
 })
