2418af83d8585c176a0e9a9d162b557af6b3f7da
Thilerion <michaelvanm@gmail.com>
2024-02-13 11:09:24 +0100

    Remove old function for finding ruleViolations / ruleConflicts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,4 @@
-export { validateBoard, findRuleConflicts } from './validate/board';
+export { validateBoard } from './validate/board';
 export * as lineValidators from './validate/line';
 export * from './generation/index.js';
 export { SimpleBoard } from './board/Board';
