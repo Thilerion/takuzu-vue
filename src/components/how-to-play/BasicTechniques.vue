cb4c24399073f33e787cb039a9f320ae43c80708
Thilerion <michaelvanm@gmail.com>
2024-02-21 18:28:08 +0100

    Basic Techniques examples

--- src/components/how-to-play/BasicTechniques.vue
+++ src/components/how-to-play/BasicTechniques.vue
@@ -6,18 +6,32 @@
 		<div class="techniques">
 			<h3>Identifying Pairs and Sandwiches</h3>
 			<p>A direct application of the "max two consecutive" rule is to avoid forming triplets within the puzzle grid.
-				This rule is pivotal in guiding you on where to place {{ valueTypeDisplayPlural }} logically.</p>
+			</p>
 
 			<h4>Pairs</h4>
 			<p>Whenever you observe two identical {{ valueTypeDisplayPlural }} next to each other, either horizontally or
 				vertically, this forms a "pair." The cells immediately adjacent to this pair must be filled with the
 				other {{ valueTypeDisplay }} to prevent creating a sequence of three identical {{ valueTypeDisplayPlural
 				}} (a triplet).</p>
+			<HowToPlayExample el="span" label="" class="w-min">
+				<template #examples>
+					<HowToPlayExampleCells :values="('.00.'.split('') as PuzzleValueLine)" />
+					<span class="">Becomes:</span>
+					<HowToPlayExampleCells :values="('1001'.split('') as PuzzleValueLine)" />
+				</template>
+			</HowToPlayExample>
 
 			<h4>Sandwiches</h4>
 			<p>A "sandwich" occurs when an empty cell is flanked by two identical {{ valueTypeDisplayPlural }}. This setup
 				mandates that the empty cell, effectively the "filling" of the sandwich, be filled with the other {{
 					valueTypeDisplay }} to avoid the formation of triplets.</p>
+			<HowToPlayExample el="span" label="" class="w-min">
+				<template #examples>
+					<HowToPlayExampleCells :values="('.1.1..'.split('') as PuzzleValueLine)" />
+					<span class="">Becomes:</span>
+					<HowToPlayExampleCells :values="('.101..'.split('') as PuzzleValueLine)" />
+				</template>
+			</HowToPlayExample>
 
 			<h3>Completing Rows and Columns</h3>
 			<p>This technique stems from the "balanced lines" rule, which stipulates that each row and column must contain
@@ -27,6 +41,13 @@
 				These rulers display how many of each {{ valueTypeDisplay }} still need to be placed in that row or column,
 				and highlight whenever a line has none remaining of a single {{ valueTypeDisplay }}, which means it can be
 				completed with the other {{ valueTypeDisplay }}.</p>
+			<HowToPlayExample el="span" label="" class="w-min">
+				<template #examples>
+					<HowToPlayExampleCells :values="('1..110'.split('') as PuzzleValueLine)" />
+					<HowToPlayExampleCells :values="('100110'.split('') as PuzzleValueLine)" />
+				</template>
+				<template #explanation>This row has 6 cells, which means there are 3 of each {{ valueTypeDisplay }} required. As you can see in the top row, there are 3 <SymbolDisplay :v="'1'" mult />, which means all other cells should be <SymbolDisplay v="0" mult/>.</template>
+			</HowToPlayExample>
 
 			<h3>Unique Rows and Columns</h3>
 			<p>This strategy is often not a necessity for solving puzzles, especially at easier difficulties, thanks to the
@@ -35,8 +56,19 @@
 			<p><strong>When to use: </strong>It's especially helpful in scenarios where a line is almost complete yet cannot
 				be completed using the basic rules of balancing, sandwiches, or pairs. If you're leveraging the rulers
 				setting that indicates the remaining number of each {{ valueTypeDisplay }} needed in a row or column, pay
-				close attention to lines that require the placement of exactly one <SymbolDisplay v="1"/> and one <SymbolDisplay v="0"/>. Compare these scenarios
-				with existing lines to identify the correct placements.</p>
+				close attention to lines that require the placement of exactly one
+				<SymbolDisplay v="1" /> and one
+				<SymbolDisplay v="0" />. Compare these scenarios
+				with existing lines to identify the correct placements.
+			</p>
+			<HowToPlayExample el="span" label="" class="w-min" v-if="false /*TODO: this example*/">
+				<template #examples>
+					<HowToPlayExampleCells :values="('1010'.split('') as PuzzleValueLine)" />
+					<HowToPlayExampleCells :values="('0101'.split('') as PuzzleValueLine)" />
+					<HowToPlayExampleCells :values="('10..'.split('') as PuzzleValueLine)" />
+					<HowToPlayExampleCells :values="('01..'.split('') as PuzzleValueLine)" />
+				</template>
+			</HowToPlayExample>
 		</div>
 	</div>
 </template>
@@ -46,6 +78,7 @@ import { computed } from 'vue';
 import { injectCellThemeData } from '../gameboard/composables/useCellThemeProvider.js';
 import { useDynamicPuzzleSymbolString } from '../dynamic-symbols/useDynamicPuzzleSymbolString.js';
 import SymbolDisplay from '@/components/dynamic-symbols/PuzzleSymbolDisplay.vue';
+import type { PuzzleValueLine } from '@/lib/types.js';
 
 const { theme: cellTheme, type: cellThemeType } = injectCellThemeData();
 const { $p } = useDynamicPuzzleSymbolString(
@@ -86,4 +119,5 @@ const valueTypeDisplayPlural = computed(() => {
 	counter-increment: sub-technique-heading 1;
 	content: counter(technique-heading) counter(sub-technique-heading, lower-alpha)".";
 	@apply absolute left-0 opacity-60;
-}</style>
+}
+</style>
--- src/components/how-to-play/HowToPlayTabsList.vue
+++ src/components/how-to-play/HowToPlayTabsList.vue
@@ -2,7 +2,7 @@
 	<nav class="mb-4 bg-white flex gap-x-2">
 		<router-link
 			class="px-3 py-2 inline-block"
-			exact-active-class="border-b-2 border-b-red-400"
+			exact-active-class="border-b-2 border-b-teal-400 text-teal-600"
 			v-for="tab in tabs"
 			:to="{ name: tab.routeName }"
 			:key="tab.label"
