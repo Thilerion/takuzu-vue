712d17f9f67444c7cc9189ef965dfc84aed35bc7
Thilerion <michaelvanm@gmail.com>
2024-03-08 15:07:10 +0100

    Fix missing DarkModeSetting current browser pref locale message

--- src/components/settings/DarkModeSetting.vue
+++ src/components/settings/DarkModeSetting.vue
@@ -17,21 +17,33 @@
 			<label class="grid radio-auto-grid">
 				<input class="col-start-1 row-start-1 mr-2" type="radio" name="radio-theme" v-model="selectedValue" value="auto">
 				<span class="col-start-2 row-start-1 self-center">{{ t('Settings.appThemeMode.auto') }}</span>
-				<span class="col-start-2 row-start-2 self-center text-xs opacity-60">{{ t('Settings.appThemeMode.autoDescription', { currentTheme: currentBrowserPreference.toLowerCase() }) }}</span>
+				<span class="col-start-2 row-start-2 self-center text-xs opacity-60">{{ t('Settings.appThemeMode.autoDescription', { currentTheme: currentBrowserPrefMessage }) }}</span>
 			</label>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { computed, ref, watch } from 'vue';
 import { useThemePreferences } from '@/composables/use-theme-preferences';
 import { debouncedWatch } from '@vueuse/core';
 import { useI18n } from 'vue-i18n';
 
-const { t } = useI18n();
+
 
 const { baseThemeUserPref: themeValue, setBaseThemePreference: setTheme, baseThemeBrowserPref: currentBrowserPreference } = useThemePreferences();
+
+const { t } = useI18n();
+const currentBrowserPrefMessage = computed(() => {
+	switch (currentBrowserPreference.value) {
+		case 'dark': return t('Settings.appThemeMode.dark').toLowerCase();
+		case 'light': return t('Settings.appThemeMode.light').toLowerCase();
+		default: {
+			const x: never = currentBrowserPreference.value;
+			return t('unknown');
+		}
+	}
+})
 
 const selectedValue = ref(themeValue.value);
 
