fbbd3923058ff6c25d686c59e3e0d6a1a505e965
Thilerion <michaelvanm@gmail.com>
2024-06-04 10:16:18 +0200

    Fix symbolDistribution quality check

--- src/lib/generation/quality.ts
+++ src/lib/generation/quality.ts
@@ -2,10 +2,9 @@ import { arrayCountValuesAsMap } from "@/utils/array.ts.utils.js";
 import type { SimpleBoard } from "../board/Board.js";
 import { ZERO, ONE } from "../constants.js";
 import type { BoardShape, DimensionStr } from "../types.js";
-import { normalize } from "@/utils/number.utils.js";
 import { getMaskRatio } from "../board/Board.helpers.js";
 
-const maskRatioTargets = new Map<DimensionStr | 'default', { min: number, optimal: number }>([
+const maskRatioTargets = new Map<DimensionStr | 'default', MaskQualityRange>([
 	['default', { min: 0.70, optimal: 0.78 }],
 
 	['4x4', { min: 0.65, optimal: 0.6875 }],
@@ -28,8 +27,10 @@ const maskRatioTargets = new Map<DimensionStr | 'default', { min: number, optima
 	['12x16', { min: 0.72, optimal: 0.78 }],
 ])
 
+export type MaskQualityRange = { min: number, optimal: number };
+
 export type GetMaskQualityCheckerOpts = {
-	maskRatioTarget?: { min: number, optimal: number },
+	maskRatioTarget?: MaskQualityRange,
 	minSymbolDistribution?: number,
 }
 export type MaskQualityResult = {
@@ -41,12 +42,16 @@ export type CheckMaskQualityOpts = {
 	/** A number between 0 and 1 that determines how strict to adhere to the "optimal" values */
 	strictness?: number,
 }
+
+/** Assesses the quality of the mask: its masking ratio, and its symbol distribution, with a specified strictness to determine if it passes the minimum values. */
+export type CheckMaskQualityFn = (board: SimpleBoard, opts?: CheckMaskQualityOpts) => MaskQualityResult;
+
 export function getMaskQualityChecker(
 	shape: BoardShape,
 	opts: GetMaskQualityCheckerOpts = {}
-) {
-	const { min, optimal } = getMaskRatioTarget(shape, opts);
-	const { minSymbolDistribution = 0.35 } = opts;
+): CheckMaskQualityFn {
+	const maskRatioTargetRange = getMaskRatioTargetRange(shape, opts);
+	const { minSymbolDistribution = 0.25 } = opts;
 
 	return (board: SimpleBoard, opts: CheckMaskQualityOpts = {}) => {
 		const {
@@ -54,17 +59,16 @@ export function getMaskQualityChecker(
 		} = opts;
 		
 		const maskRatio = getMaskRatio(board);
-		const symbolDistribution = getSymbolDistributionQuality(board);
+		const symbolDistribution = getSymbolDistributionRatio(board);
 
 		let success = true;
 
 		// Determine the absolute minimum mask ratio, based on the strictness (where 1 means it must be at least "optimal", and 0 means it must be at least "min")
-		const minMaskRatio = min + (optimal - min) * strictness;
+		const minMaskRatio = getMinQualityWithStrictness(maskRatioTargetRange, strictness);
 
 		if (maskRatio < minMaskRatio) {
 			success = false;
-		}
-		if (symbolDistribution < minSymbolDistribution) {
+		} else if (symbolDistribution < minSymbolDistribution) {
 			success = false;
 		}
 
@@ -76,8 +80,8 @@ export function getMaskQualityChecker(
 	}
 }
 
-/** Gives the (normalized) symbol distribution, with 0 meaning all given values are the same, and 1 meaning they are equally distributed. */
-function getSymbolDistributionQuality(board: SimpleBoard): number {
+/** Gives the (lowest) symbol distribution between 0 and 0.5, with 0 meaning all given values are the same, and 0.5 meaning they are equally distributed. */
+function getSymbolDistributionRatio(board: SimpleBoard): number {
 	const symbolCounts = arrayCountValuesAsMap([...board.grid].flat());
 	const countZero = symbolCounts.get(ZERO)!;
 	const countOne = symbolCounts.get(ONE)!;
@@ -88,10 +92,10 @@ function getSymbolDistributionQuality(board: SimpleBoard): number {
 	
 	// Ratio of the least common symbol. Best if higher than 0.35, with a max of 0.5 possible when both symbols are equally given.
 	const symbolRatio = Math.min(symbolRatioOne, symbolRatioZero);
-	return normalize(symbolRatio, 0, 0.5);
+	return symbolRatio;
 }
 
-function getMaskRatioTarget(shape: BoardShape, opts?: GetMaskQualityCheckerOpts): { min: number, optimal: number } {
+function getMaskRatioTargetRange(shape: BoardShape, opts?: GetMaskQualityCheckerOpts): MaskQualityRange {
 	if (opts?.maskRatioTarget) {
 		return opts.maskRatioTarget;
 	}
@@ -100,4 +104,8 @@ function getMaskRatioTarget(shape: BoardShape, opts?: GetMaskQualityCheckerOpts)
 		key = 'default';
 	}
 	return maskRatioTargets.get(key)!;
+}
+
+function getMinQualityWithStrictness(qualityRange: MaskQualityRange, strictness: number) {
+	return qualityRange.min + (qualityRange.optimal - qualityRange.min) * strictness;
 }
