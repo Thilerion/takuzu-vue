a4a2bdc4b876ba7e45bc5f1f2cd5ca1bb4a867e8
Thilerion <michaelvanm@gmail.com>
2024-06-13 13:16:11 +0200

    Remove old puzzle input components

--- src/components/puzzle-input/types.ts
+++
@@ -1,5 +0,0 @@
-import type { PuzzleSymbol } from "@/lib/constants.js";
-import type { Grid } from "@/lib/types.js";
-
-export type PuzzleInputValue = PuzzleSymbol | '';
-export type PuzzleInputGrid = Grid<PuzzleInputValue>;
