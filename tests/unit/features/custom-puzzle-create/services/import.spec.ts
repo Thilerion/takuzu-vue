aa2723877ad90e0a0c6685a794b610ff51cfc50d
Thilerion <michaelvanm@gmail.com>
2024-06-15 16:08:35 +0200

    Import custom puzzle string tests

---
+++ tests/unit/features/custom-puzzle-create/services/import.spec.ts
@@ -0,0 +1,52 @@
+import { importCustomPuzzleString, type ParsedCustomPuzzleString } from "@/features/puzzle-editor/services/string-conversions/import.js";
+import type { BoardExportString, BoardString } from "@/lib/types.js"
+
+describe('import custom puzzle string', () => {
+	it('can import a BoardString', () => {
+		const boardString = '0..1..1.0.10....' as BoardString;
+		const result = importCustomPuzzleString(boardString);
+		expect(result).toMatchObject({
+			success: true,
+			width: 4,
+			height: 4,
+			type: 'BoardString'
+		})
+		expect((result as ParsedCustomPuzzleString).grid.flat().join('')).toBe(boardString);
+	})
+
+	it('can import a BoardExportString', () => {
+		const boardStr = '0..1....1...0.10........';
+		const str = `6x4;${boardStr}` as BoardExportString;
+		const result = importCustomPuzzleString(str);
+		expect(result).toMatchObject({
+			success: true,
+			width: 6,
+			height: 4,
+			type: 'BoardExportString'
+		})
+		expect((result as ParsedCustomPuzzleString).grid.flat().join('')).toBe(boardStr);
+	})
+
+	it('can import a CustomLong string', () => {
+		const str = '0..1.. ..1... 0.10.. ......';
+		const result = importCustomPuzzleString(str);
+		expect(result).toMatchObject({
+			success: true,
+			width: 6,
+			height: 4,
+			type: 'CustomLong'
+		})
+		expect((result as ParsedCustomPuzzleString).grid.flat().join('')).toBe('0..1....1...0.10........');
+	})
+
+	it('can import a CustomRLE string', () => {
+		const result = importCustomPuzzleString('1b5d5c1a51h');
+		expect(result).toMatchObject({
+			success: true,
+			width: 6,
+			height: 4,
+			type: 'CustomRLE'
+		})
+		expect((result as ParsedCustomPuzzleString).grid.flat().join('')).toBe('0..1....1...0.10........');
+	})
+})
