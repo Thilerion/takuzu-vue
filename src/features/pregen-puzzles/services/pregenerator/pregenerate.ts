1f1dfd4e3a5d84054b7b97d99d9841623c627e68
Thilerion <michaelvanm@gmail.com>
2024-07-17 13:15:12 +0200

    Improved pregenerate error handling

--- src/features/pregen-puzzles/services/pregenerator/pregenerate.ts
+++ src/features/pregen-puzzles/services/pregenerator/pregenerate.ts
@@ -21,29 +21,28 @@ export async function generateMissingPuzzles(
 
 	let attemptsLeft = maxRetries;
 	let failed: PregenPresetConfig[] = [];
-	// Reverse presets to get currentPresets, to allowing taking the last item inside the loop
-	let currentPresets = [...presets].reverse();
+	let currentPresets = [...presets];
 	const successes: IPregenPuzzle[] = []; // Save the generated puzzles here
 
 	const successfulPresetKeys: PuzzleConfigKey[] = [];
 
 	while (attemptsLeft > 0 && currentPresets.length > 0) {
 		attemptsLeft -= 1;
-		while (currentPresets.length) {
-			if (lazy) {
-				// prevent fans blowing etc when first opening the page
-				await awaitTimeout(250);
-			}
-			// Take the last item from the "currentPresets" array. So if we want to preserve the ordering of the input presets array, this needs to have been reversed
-			const cur = currentPresets.pop()!;
-			const result = await genPuzzleFn(cur.config);
-			if (result == null || !result.boardStr) {
-				failed.push(cur);
-				continue;
-			}
-			successes.push(result);
-			successfulPresetKeys.push(cur.key);
+		
+		try {
+			const runResult = await generateMissingPuzzlesSingleRun(
+				currentPresets,
+				genPuzzleFn,
+				{ sleep: lazy ? 250 : null }
+			);
+			successes.push(...runResult.results);
+			successfulPresetKeys.push(...runResult.successes);
+			failed = [...failed, ...runResult.failures];
+		} catch(e) {
+			console.warn('Error while pre-generating puzzles:', e);
+			console.warn(`There are ${attemptsLeft} attempts left, and ${currentPresets.length} presets left to generate.`);
 		}
+
 		if (attemptsLeft > 0) {
 			// Only if attemptsLeft, so "failures" are included correctly
 			currentPresets = [...failed];
@@ -55,5 +54,55 @@ export async function generateMissingPuzzles(
 		results: successes,
 		successes: successfulPresetKeys,
 		failures: failed.map(f => f.key)
+	}
+}
+
+/** Does a single run through the presets to generate, and returns the generated puzzles, their presets, and any failed presets. */
+async function generateMissingPuzzlesSingleRun(
+	presets: PregenPresetConfig[],
+	genPuzzleFn: GeneratePuzzleFn,
+	options: { sleep: null | number}
+): Promise<{
+	results: IPregenPuzzle[],
+	successes: PuzzleConfigKey[],
+	failures: PregenPresetConfig[]
+}> {
+	const { sleep } = options;
+	const lazy = sleep != null;
+
+	const failed: PregenPresetConfig[] = [];
+	const successes: IPregenPuzzle[] = []; // Save the generated puzzles here
+	const successfulPresetKeys: PuzzleConfigKey[] = [];
+
+	let genPuzzleErrors = 0;
+
+	for (let i = 0; i < presets.length; i++) {
+		if (lazy) {
+			// prevent fans blowing etc when first opening the page
+			await awaitTimeout(250);
+		}
+		// Take the last item from the "currentPresets" array. So if we want to preserve the ordering of the input presets array, this needs to have been reversed
+		const cur = presets[i];
+		const result = await genPuzzleFn(cur.config).catch(() => {
+			genPuzzleErrors += 1;
+			return null;
+		});
+		
+		if (result == null || !result.boardStr) {
+			failed.push(cur);
+			continue;
+		}
+		successes.push(result);
+		successfulPresetKeys.push(cur.key);
+
+		if (genPuzzleErrors >= 3) {
+			throw new Error(`Generate puzzle function threw 3 errors. Something is wrong.`);
+		}
+	}
+
+	return {
+		results: successes,
+		successes: successfulPresetKeys,
+		failures: failed
 	}
 }
