6dace55719474a42d193972219b0b9e3ae45b333
Thilerion <michaelvanm@gmail.com>
2024-07-20 17:07:12 +0200

    Remove old Puzzle analysis worker

--- src/views/analysis/PuzzleAnalysis.vue
+++ src/views/analysis/PuzzleAnalysis.vue
@@ -7,32 +7,14 @@
 		class="flex-shrink-0"
 	>
 		Puzzle Analysis
-		<template
-			v-if="debugMode"
-			#right
-		>
-			<IconBtn @click="showAnalysisWorkerTest = !showAnalysisWorkerTest"><icon-gg-debug /></IconBtn>
-		</template>
 	</PageHeader>
 	<div class="flex-1 pt-4">
 		<main class="gap-y-4 grid bleed-grid-4 v-grid-bleed text-sm">
-			<DebugAnalysisWorkerTest v-if="showAnalysisWorkerTest" />
+			<p>Puzzle analysis is not yet implemented. Sorry!</p>
 		</main>
 	</div>
 </div>
 </template>
-
-<script setup lang="ts">
-import { useMainStore } from '@/stores/main';
-import { defineAsyncComponent, ref, toRef } from 'vue';
-
-const DebugAnalysisWorkerTest = defineAsyncComponent(() => 
-	import('./DebugAnalysisWorkerTest.vue')
-)
-const showAnalysisWorkerTest = ref(false);
-const mainStore = useMainStore();
-const debugMode = toRef(mainStore, 'debugMode');
-</script>
 
 <style scoped>
 .v-grid-bleed {
